5eb0e7f7195fac82c9b7aaab87787c29551a3f28
lecopivo <skrivantomas@seznam.cz>
2021-09-30 22:43:47 +0200

    ...

--- SciLean/Linear/Tests.lean
+++ SciLean/Linear/Tests.lean
@@ -22,26 +22,3 @@ theorem test9  : IsLin λ u : X => u + ((3 : ℝ) * u + u) + u := by rmlamlet; i
 theorem test10 (A : X → X) [IsLin A] : IsLin λ u : X => (u + ((u + A u) + (10 : ℝ)*u) + u) + u := by rmlamlet; infer_instance
 theorem test11 (A : X → X) (B : X → X) [IsLin A] [IsLin B] : IsLin λ x => A x + B (A (B x) + B x) := by rmlamlet; infer_instance
 
-set_option trace.Meta.synthInstance true
-
--- theorem test8 (w : U) : IsLin λ u : U => ⟨u, u⟩ := by rmlamlet; infer_instance
-
-
--- theorem ttest9 : (λ x y : U => x + y) = 0 := by rmlamlet; admit
--- theorem ttest10 : (λ x y : U => y + x) = 0 := by rmlamlet; admit
-
--- theorem ttest11 (f : X → Y) : (λ x y => f x + y) = 0 := by rmlamlet; admit
--- theorem ttest12 (f : X → Y) : (λ x y => x + f y) = 0 := by rmlamlet; admit
--- theorem ttest13 (f : X → Y) : (λ x y => f y + x) = 0 := by rmlamlet; admit
--- theorem ttest14 (f : X → Y) : (λ x y => y + f x) = 0 := by rmlamlet; admit
-
-
--- theorem ttest15 (f : X → Z) (f' : Y → Z) : (λ x y => f x + f' y) = 0 := by rmlamlet; admit
--- theorem ttest16 (f : X → Z) (f' : Y → Z) : (λ x y => f' x + f y) = 0 := by rmlamlet; admit
--- theorem ttest17 (f : X → Z) (f' : Y → Z) : (λ x y => f y + f' x) = 0 := by rmlamlet; admit
--- theorem ttest18 (f : X → Z) (f' : Y → Z) : (λ x y => f' y + f x) = 0 := by rmlamlet; admit
-
-
--- theorem ttest9 (f : X → Z) (g : Y → Z) : (λ x y => f x + y) = 0 := by rmlamlet; admit
--- theorem test9 (f : X → Z) (g : Y → Z) : (λ x y => f x + g y) = 0 := by rmlamlet; admit
--- theorem test9 (f : X → Z) (g : Y → Z) : (λ x y => g y + f x) = 0 := by rmlamlet; admit
