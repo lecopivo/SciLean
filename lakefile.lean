e931e5e2cb6179789b8536392983ed50e6638631
Tomas Skrivan <tomass@sidefx.com>
2022-01-14 12:45:48 +0100

    update to lean and mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL System
 package SciLean {
   dependencies := #[{
     name := `mathlib
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "8090594"
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "089ae0a"
   }]
 }
 
