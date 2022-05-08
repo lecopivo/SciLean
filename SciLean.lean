e74d97d603032768cf44e9f7cea84eb3c475f273
lecopivo <skrivantomas@seznam.cz>
2022-05-08 18:44:17 +0200

    ...

--- SciLean.lean
+++ SciLean.lean
@@ -1,1 +1,8 @@
-import SciLean.Basic
+import SciLean.Mechanics
+import SciLean.Operators.ODE
+import SciLean.Solver 
+import SciLean.Tactic.LiftLimit
+import SciLean.Tactic.FinishImpl
+import SciLean.Data.PowType
+import SciLean.Core.Extra
+
