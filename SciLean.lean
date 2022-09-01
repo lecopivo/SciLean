2555811c2f74d65738370945594165c88e26e20b
lecopivo <skrivantomas@seznam.cz>
2022-09-02 01:57:07 +0200

    first try to install Alectryon and LeanInk

--- SciLean.lean
+++ SciLean.lean
@@ -17,3 +17,49 @@ import SciLean.Tactic.FinishImpl
 -- import SciLean.Notation
 -- import SciLean.Functions
 
+
+/-!
+
+hohohoho
+========
+
+heheh 
+=====
+
+
+hihihi
+------
+
+
+asdf
+
+.. math:  \int e^x
+
+
+\(
+\int e^x
+)\
+
+
+$$
+\int e^x
+$$
+
+some Lean code
+
+```lean
+
+#check Nat
+
+
+```
+
+
+```lean
+def compose.{u, v, w}
+            {α : Type u} {β : Type v} {γ : Type w}
+            (g : β → γ) (f : α → β) (x : α) : γ :=
+  g (f x)
+```
+
+ -/ 
