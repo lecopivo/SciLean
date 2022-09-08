60687cc6defb37dd682d0a67dd1f0818ea85c63c
lecopivo <skrivantomas@seznam.cz>
2022-09-08 21:42:37 +0200

    update to 'remove lambda let' tactic

--- SciLean/Tactic/RemoveLambdaLet.lean
+++ SciLean/Tactic/RemoveLambdaLet.lean
@@ -6,6 +6,13 @@ import SciLean.Prelude
 
 import Lean.Elab.Tactic.Conv.Basic
 
+namespace Function
+
+  def scomb {α β γ} (f : α → β → γ) (g : α → β) (x : α) := f x (g x)
+  def diag {α β} (f : α → α → β) (x : α) := f x x
+
+end Function
+
 namespace Lean.Elab.Tactic.Conv
 open Meta
 
@@ -17,31 +24,31 @@ open Lean.Elab.Tactic
 def extractfvar (e : Expr) (v : Expr) (lctx : LocalContext) : MetaM Expr := do
 withLCtx lctx #[] do
 let V ← inferType v
-let fvarid ← v.fvarId!
+let fvarid := v.fvarId!
 let E ← inferType e
 let u ← getLevel E
 match e with
-  | Expr.fvar fvarid' _ => if (fvarid'==fvarid) then pure (mkApp (mkConst ``id [u]) E) else (mkAppM `const #[V, e])
-  | Expr.app f x _ => 
+  | Expr.fvar fvarid' => if (fvarid'==fvarid) then pure (mkApp (mkConst ``id [u]) E) else (mkAppM ``Function.const #[V, e])
+  | Expr.app f x => 
     match (f.containsFVar fvarid), (x.containsFVar fvarid) with
-      | false, false => mkAppM `const #[V, e]
-      | false, true => if (x==v) then f else mkAppM `comp #[f, (← extractfvar x v lctx)]
-      | true, false => mkAppM `swap #[(← extractfvar f v lctx), x]
+      | false, false => mkAppM ``Function.const #[V, e]
+      | false, true => if (x==v) then pure f else mkAppM ``Function.comp #[f, (← extractfvar x v lctx)]
+      | true, false => mkAppM ``Function.swap #[(← extractfvar f v lctx), x]
       | true, true => -- mkAppM `subs #[(← extractfvar f v lctx), (← extractfvar x v lctx)]
                       if (x==v) then 
-                        mkAppM `diag #[(← extractfvar f v lctx)]
+                        mkAppM ``Function.diag #[(← extractfvar f v lctx)]
                       else
-                        mkAppM `subs #[(← extractfvar f v lctx), (← extractfvar x v lctx)]
-  | e => e    
+                        mkAppM ``Function.scomb #[(← extractfvar f v lctx), (← extractfvar x v lctx)]
+  | e => pure e    
 
 partial def removelambdalet (e : Expr) (lctx : LocalContext) : MetaM Expr :=
 withLCtx lctx #[] do
 match e with
- | Expr.app f x _ => do mkApp (← removelambdalet f lctx) (← removelambdalet x lctx)
+ | Expr.app f x => pure $ mkApp (← removelambdalet f lctx) (← removelambdalet x lctx)
  | Expr.lam .. => lambdaTelescope e fun xs b => do
    let xs := xs.reverse
    let mut b ← removelambdalet b (← getLCtx)
-   let B ← inferType b
+   -- let B ← inferType b
    for x in xs do
      -- if ¬(B.containsFVar x.fvarId!) then
      b ← extractfvar b x (← getLCtx)
@@ -53,27 +60,27 @@ match e with
      let fvarId ← mkFreshFVarId
      let lctx := lctx.mkLetDecl fvarId n t v
      let fvar := mkFVar fvarId
-     let b ← b.instantiate #[fvar]
+     let b := b.instantiate #[fvar]
      let b ← removelambdalet b lctx
      let b ← extractfvar b fvar lctx
      let v ← removelambdalet v lctx
-     mkApp b v
- | e' => e'
+     pure $ mkApp b v
+ | e' => pure e'
 
 syntax (name := rmlamlet) "rmlamlet" : tactic
 
 def rmlamletCore (mvarId : MVarId) : MetaM (List MVarId) :=
-  withMVarContext mvarId do
-    let tag      ← getMVarTag mvarId
-    let target   ← getMVarType mvarId
-    let u        ← getLevel target
+  mvarId.withContext do
+    let tag      ← mvarId.getTag
+    let target   ← mvarId.getType
+    -- let u        ← getLevel target
     let targetNew ← (removelambdalet target (← getLCtx))
     let mvarNew  ← mkFreshExprSyntheticOpaqueMVar targetNew tag
-    let eq       ← mkEq target targetNew
-    let eqMvar  ← mkFreshExprSyntheticOpaqueMVar eq
-    let val  := mkAppN (Lean.mkConst `Eq.mpr [u]) #[target, targetNew, eqMvar, mvarNew]
+    -- let eq       ← mkEq target targetNew
+    -- let eqMvar  ← mkFreshExprSyntheticOpaqueMVar eq
+    -- let val  := mkAppN (Lean.mkConst `Eq.mpr [u]) #[target, targetNew, eqMvar, mvarNew]
     -- assignExprMVar mvarId var
-    assignExprMVar mvarId mvarNew
+    mvarId.assign mvarNew
     return [mvarNew.mvarId!]
 
 @[tactic rmlamlet] def tacticRemoveLambdaLet : Tactic
@@ -91,6 +98,12 @@ fun stx => withMainContext do
           changeLhs (← removelambdalet lhs (← getLCtx))
           pure ()
 
+
+example : (λ x : Nat => x + x) = (λ _ => 0) := 
+by 
+  rmlamlet
+  admit
+  done
 
 -- syntax (name := print_goal) "print_goal" : conv
 
