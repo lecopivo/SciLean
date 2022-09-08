9b51d21833b5490ed68bff157f473f5c2543ec7a
lecopivo <skrivantomas@seznam.cz>
2022-09-08 14:59:28 +0200

    Derivative of odeSolve w.r.t. the function `f`

--- SciLean/Functions/OdeSolve.lean
+++ SciLean/Functions/OdeSolve.lean
@@ -2,6 +2,9 @@ import SciLean.Core
 
 namespace SciLean
 
+-- TODO: Add Semi Group property for `f` that guarantees the existence
+--       of solution for all times
+
 noncomputable
 def odeSolve {X} [Vec X] (f : ℝ → X → X) (t : ℝ) (x₀ : X) : X := sorry
 
@@ -20,7 +23,7 @@ argument x₀ [Hilbert X] [IsSmooth f] [∀ s, HasAdjoint (f s)]
   adj_simp   := odeSolve (λ s => (f (t - s))†) t x₀' 
   by 
     -- Define adjoint solution `y such that
-    --  ∀ s, ⟪x (t - s), y s⟫ = ⟪x t, y 0⟫ 
+    --  ∀ s, ⟪x (t - s), y s⟫ = ⟪x t, y 0⟫
     -- in particular for s := t we get desired ⟪x 0, y t⟫ = ⟪x t, y 0⟫
     -- Differentiate above equation w.r.t to `s and you get that `y satisfies
     -- ∂ y s 1 = (f (t - s))†
@@ -39,12 +42,23 @@ argument x₀ [Hilbert X] [IsSmooth f] [inst : ∀ t, HasAdjDiff (f t)]
       have isf := λ t => (inst t).isSmooth
       have iaf := λ t => (inst t).hasAdjDiff
       simp at iaf
-      admit
-      -- simp[adjointDifferential] done
+      simp[adjointDifferential]
+      done
 
 
-  
+instance odeSolve.arg_f.isSmooth {X W} [Vec X] [Vec W] 
+  (f : W → ℝ → X → X) [IsSmooth f] [∀ w, IsSmooth (f w)] [∀ w t, IsSmooth (f w t)]
+  : IsSmooth (λ w => odeSolve (f w)) := sorry
+
+@[simp]
+theorem odeSolve.arg_f.diff_simp {X W} [Vec X] [Vec W] 
+  (f : W → ℝ → X → X) [IsSmooth f] [∀ w, IsSmooth (f w)] [∀ w t, IsSmooth (f w t)]
+  : ∂ (λ w => odeSolve (f w))
+    =
+    λ w dw t x => (odeSolve (λ t (x,dx) => (f w t x, ∂ f w dw t x + ∂ (f w t) x dx)) t (x,0)).1
+  := sorry
+
 -- example [Hilbert X] (f : ℝ → X → X) (y : X) [IsSmooth f] [∀ t, HasAdjDiff (f t)] 
 --   : ∇ (λ x₀ => ∥odeSolve f t x₀ - y∥²) = 0 := 
 -- by 
---   simp; unfold hold; simp
+--   simp[gradient]; unfold hold; simp
