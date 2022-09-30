8f0eaf38e8f9ad995ae88c18a87ccbcbfcc5286a
lecopivo <skrivantomas@seznam.cz>
2022-09-30 12:30:14 +0200

    Product of PODs seems to be working now

--- SciLean/Data/DataArray/DataArray.lean
+++ SciLean/Data/DataArray/DataArray.lean
@@ -5,15 +5,42 @@ namespace SciLean
 
 -- TODO: Quotient it out by trailing bits
 structure DataArray (α : Type) [pd : PlainDataType α] where
-  data : ByteArray
+  byteData : ByteArray
   size : Nat 
-  h_size : pd.bytes size ≤ data.size
+  h_size : pd.bytes size ≤ byteData.size
 
 variable {α : Type} [pd : PlainDataType α]
 variable {ι} [Enumtype ι]
 
-def DataArray.get (a : DataArray α) (i : Fin a.size) : α := pd.get a.data i sorry_proof
-def DataArray.set (a : DataArray α) (i : Fin a.size) (val : α) : DataArray α := ⟨pd.set a.data i sorry_proof val, a.size, sorry_proof⟩
+def DataArray.get (arr : DataArray α) (i : Fin arr.size) : α := -- pd.get a.data i sorry_proof
+  match pd.btype with
+  | .inl bitType => 
+    let perByte := 8/bitType.bits
+    let inByte  := (i.1 % perByte.toNat).toUInt8
+    let ofByte  : Fin arr.byteData.size := ⟨i.1 / perByte.toNat, sorry_proof⟩
+    let ones : UInt8 := 255
+    let mask    := (ones - (ones <<< bitType.bits))   -- 00000111 
+    -- masking is note necessary if `fromBytes` correctly ignores unused bits
+    let byte    := mask &&& (arr.byteData[ofByte] >>> (inByte*bitType.bits))
+    bitType.fromByte byte
+  | .inr byteType => 
+    byteType.fromByteArray arr.byteData (byteType.bytes * i.1) sorry_proof
+
+def DataArray.set (arr : DataArray α) (i : Fin arr.size) (val : α) : DataArray α := -- ⟨pd.set a.byteData i sorry_proof val, a.size, sorry_proof⟩
+  match pd.btype with
+  | .inl bitType => 
+    let perByte := 8/bitType.bits
+    let inByte  := (i.1 % perByte.toNat).toUInt8
+    let ofByte  := ⟨i.1 / perByte.toNat, sorry_proof⟩
+    let ones : UInt8 := 255
+    let mask    := ones - ((ones - (ones <<< bitType.bits)) <<< (inByte*bitType.bits))  --- 11000111 for bitType.bits = 3 and inByte = 1
+    let byte    := arr.byteData[ofByte]
+    let newByte := (mask &&& byte) + (bitType.toByte val <<< (inByte*bitType.bits))
+    ⟨arr.byteData.set ofByte newByte, arr.size, sorry_proof⟩
+  | .inr byteType => 
+    ⟨byteType.toByteArray arr.byteData (byteType.bytes * i.1) sorry_proof val, arr.size, sorry_proof⟩
+
+
 
 /-- Extensionality of DataArray
 
@@ -90,4 +117,25 @@ instance : FunType.HasIntro (DataArrayN α (numOf ι)) where
 
 instance : PowType (DataArrayN α (numOf ι)) ι α := ⟨⟩
 
-#check ℝ^{3}
+#check ℝ^(Fin 3)
+
+#eval ((λ [i] => i.1.toReal ) : ℝ^(Fin 100)) |>.map Math.sqrt
+
+#eval ((λ [i] => i) : (Fin 3 × Fin 5)^(Fin 3 × Fin 5))
+#eval ((λ [i] => i) : (Fin 3 × Fin 5)^(Fin 3 × Fin 5)).data
+#eval ((λ [i] => i) : (Fin 3 × Fin 5)^(Fin 3 × Fin 5)).data.byteData
+
+#eval (1,2,3,4)
+
+#eval ((λ [i] => i) : (Fin 3 × Fin 2 × Fin 2)^(Fin 3 × Fin 2 × Fin 2))
+#eval ((λ [i] => i) : (Fin 3 × Fin 2 × Fin 2)^(Fin 3 × Fin 2 × Fin 2)).data
+#eval ((λ [i] => i) : (Fin 3 × Fin 2 × Fin 2)^(Fin 3 × Fin 2 × Fin 2)).data.byteData
+
+#eval ((λ [i] => (i.1.toFloat, i.1.toFloat.sqrt)) : (Float × Float)^(Fin 3))
+
+#eval Id.run do
+  let mut a : ℝ^{100} := λ [i] => i.1
+  for (i,_) in Enumtype.fullRange (Fin 100) do
+    a[i] *= 1000 
+    a[i] += Math.sqrt i.1
+  a
--- SciLean/Data/DataArray/PlainDataType.lean
+++ SciLean/Data/DataArray/PlainDataType.lean
@@ -1,27 +1,41 @@
 import SciLean.Prelude
+import SciLean.Algebra
 
 namespace SciLean
 
-inductive BitSize where | bits (n : UInt8) (h : n < 8) | bytes (n : Nat)
-deriving Inhabited
+structure BitType (α : Type) where
+  bits : UInt8
+  h_size : bits ≤ 8  -- we consider only types fitting into a single byte
+  fromByte (b : UInt8) : α
+  toByte   (a : α) : UInt8
 
-/-- At least `i` elements of size `s` fit into `a` -/
-def BitSize.InBounds (s : SciLean.BitSize) (a : ByteArray) (i : Nat)  : Prop :=
-match s with
-| .bits n _ => i < (8/n).toNat * a.size
-| .bytes n  => n*i < a.size
+  -- TODO: Add condition that toByte sets all unused bits to zero
+  -- TODO: Add condition that fromByte does not use any of the unused bits
+  fromByte_toByte : ∀ a, fromByte (toByte a) = a
 
-/-- This rougly corresponds to Plain Old Data/Passive Data known from OOP
+structure ByteType (α : Type) where
+  bytes : Nat
+  h_size : 1 < bytes  -- for one byte types use BitInfo
+  fromByteArray (b : ByteArray) (i : Nat) (h : i+bytes ≤ b.size) : α
+  toByteArray   (b : ByteArray) (i : Nat) (h : i+bytes ≤ b.size) (a : α) : ByteArray
+
+  -- `toByteArray` does not modify ByteArray size
+  toByteArray_size : ∀ b i h a, (toByteArray b i h a).size = b.size
+  -- we can recover `a` from bytes
+  fromByteArray_toByteArray : ∀ a b i h h', fromByteArray (toByteArray b i h a) i h' = a
+  -- `toByteArray` does not affect other bytes
+  fromByteArray_toByteArray_other : ∀ a b i j h, (j < i) ∨ (i+size) ≤ j → (toByteArray b i h a).get! j = b.get! j
+
+/-- This rougly corresponds to Plain Old Data(POD)/Passive Data known from OOP
 
 wiki: https://en.wikipedia.org/wiki/Passive_data_structure
 
-For example and array of fixed length `{a : Array α // a.size = n}` is `PlainDataType` (if `α` is `PlainDataType`)
-However, `Array α` is not `PlainDataType` (even if `α` is `PlainDataType`) as it does not have a fixed byte size.
+We distinguish between two main types of POD. `BitType` a type that is smaller or equal to a byte and `ByteType` that takes up multiple bytes. The main motivation is an efficient storage of `Array Bool` where `Bool` takes up only a single bit, so we can fit 8 bools into a single byte and achieve significant memore reduction.
+
+Potentially surprising edge case is array of fixed length, i.e. the type `{a : Array α // a.size = n}`. It is `PlainDataType` if `α` is `PlainDataType`. However, `Array α` is not `PlainDataType`, even if `α` is `PlainDataType`, as it does not have a fixed byte size. 
 -/
 class PlainDataType (α : Type) where
-  size : BitSize
-  get (d : ByteArray) (i : Nat) (h : size.InBounds d i) : α
-  set (d : ByteArray) (i : Nat) (h : size.InBounds d i) (a : α) : ByteArray
+  btype : BitType α ⊕ ByteType α
 
   -- get_set       -- setting and getting returns the original
   -- get_set_other -- set does not affects other elements
@@ -30,100 +44,267 @@ class PlainDataType (α : Type) where
 
 /- How many bytes are needed to hold n elements of type α -/
 def PlainDataType.bytes {α : Type} (pd : PlainDataType α) (n : Nat) : Nat :=
-  match pd.size with
-  | .bits m _ => (n + ((8/m) - 1).toNat) / (8/m).toNat
-  | .bytes m => m * n
+  match pd.btype with
+  | .inl bitType => (n + ((8/bitType.bits) - 1).toNat) / (8/bitType.bits).toNat
+  | .inr byteType => byteType.bytes * n
 
 
 --------------- Prod -------------------------------------------------
 ----------------------------------------------------------------------
 
-def BitSize.ofProd (α β : Type) [pda : PlainDataType α] [pdb : PlainDataType β] : BitSize :=
-  match pda.size, pdb.size with
-  | .bits n _, .bits m _ => 
-    if  h : n + m < 8  then .bits (n+m) h
-    else if n + m = 8  then .bytes 1    else .bytes 2
-  | .bits _ _, .bytes n | .bytes n, .bits _ _ => .bytes (n+1)
-  | .bytes n, .bytes m => .bytes (n+m)
+def Prod.bitTypeProd {α β} (ta : BitType α) (tb : BitType β) : BitType (α × β) ⊕ ByteType (α × β) :=
+  if h : ta.bits + tb.bits ≤ 8 then
+    .inl {
+      bits   := ta.bits + tb.bits
+      h_size := h
+
+      fromByte := λ byte => 
+        -- Maybe the mask is not necessary of `fromByte` correctly ignores unused bits
+        let ones  := (255 : UInt8)
+        let aMask := ones - (ones <<< ta.bits)               -- e.g. 00000111   
+        let bMask := (ones - (ones <<< tb.bits)) <<< ta.bits -- e.g. 00011000
+        (ta.fromByte (aMask &&& byte), tb.fromByte ((bMask &&& byte) >>> ta.bits))
+      toByte   := λ (a,b) => 
+        -- let ones  := (255 : UInt8)
+        -- let aMask := ones - (ones <<< ta.bits)               -- e.g. 00000111   
+        -- let bMask := (ones - (ones <<< tb.bits)) <<< ta.bits -- e.g. 00011000
+        let aByte := ta.toByte a
+        let bByte := tb.toByte b
+        -- Masking is not necessary if `toByte` correctly sets unused bits to zero
+        aByte /- &&& aMask -/ + (bByte <<< ta.bits) /- &&& bMask -/
+        
+      fromByte_toByte := sorry_proof
+    }
+  else
+    .inr {
+      bytes := 2
+      h_size := by simp done
+
+      fromByteArray := λ b i _ => 
+        let aByte := b[2*i]'sorry_proof
+        let bByte := b[2*i+1]'sorry_proof
+        (ta.fromByte aByte, tb.fromByte bByte)
+      toByteArray := λ arr i _ (a,b) =>
+        arr |>.set ⟨  2*i, sorry_proof⟩ (ta.toByte a)
+            |>.set ⟨2*i+1, sorry_proof⟩ (tb.toByte b)
+
+      toByteArray_size := sorry_proof
+      fromByteArray_toByteArray := sorry_proof
+      fromByteArray_toByteArray_other := sorry_proof
+    }
+
+def Prod.bitTypeByteTypeProd {α β} (ta : BitType α) (tb : ByteType β) : ByteType (α × β) :=
+  {
+    bytes := tb.bytes + 1
+    h_size := sorry_proof
+
+    fromByteArray := λ arr i _ => 
+      let aByte := arr[i]'sorry_proof
+      (ta.fromByte aByte, tb.fromByteArray arr (i+1) sorry_proof)
+    toByteArray := λ arr i _ (a,b) =>
+      arr |>.set ⟨i, sorry_proof⟩ (ta.toByte a)
+          |> (tb.toByteArray · (i+1) sorry_proof b)
+
+    toByteArray_size := sorry_proof
+    fromByteArray_toByteArray := sorry_proof
+    fromByteArray_toByteArray_other := sorry_proof
+  }
+
+def Prod.byteTypeBitTypeProd {α β} (ta : ByteType α) (tb : BitType β) : ByteType (α × β) :=
+  {
+    bytes := ta.bytes + 1
+    h_size := sorry_proof
+
+    fromByteArray := λ arr i _ => 
+      let bByte := arr[i + ta.bytes]'sorry_proof
+      (ta.fromByteArray arr i sorry_proof, tb.fromByte bByte)
+    toByteArray := λ arr i _ (a,b) =>
+      arr |> (ta.toByteArray · i sorry_proof a)
+          |>.set ⟨i + ta.bytes, sorry_proof⟩ (tb.toByte b)
+
+    toByteArray_size := sorry_proof
+    fromByteArray_toByteArray := sorry_proof
+    fromByteArray_toByteArray_other := sorry_proof
+  }
+
+
+def Prod.byteTypeProd {α β} (ta : ByteType α) (tb : ByteType β) : ByteType (α × β) :=
+  {
+    bytes := ta.bytes + tb.bytes
+    h_size := sorry_proof
+
+    fromByteArray := λ arr i _ => 
+      (ta.fromByteArray arr i sorry_proof,
+       tb.fromByteArray arr (i+ta.bytes) sorry_proof)
+    toByteArray := λ arr i _ (a,b) =>
+      arr |> (ta.toByteArray · (i) sorry_proof a)
+          |> (tb.toByteArray · (i+ta.bytes) sorry_proof b)
+
+    toByteArray_size := sorry_proof
+    fromByteArray_toByteArray := sorry_proof
+    fromByteArray_toByteArray_other := sorry_proof
+  }
+
+/-- Product of `PlainDataType` is `PlainDataType`
+
+**Instance diamond** This instance is currently prefered over `instPlainDataTypeEnumtype`.
+
+This instance makes a diamond together with `instPlainDataTypeEnumtype`  when `α` and `β` are `Enumtype`. Using this instance is less computationally intensive when writting and reading from `DataArra` but it consumes more memory. The `instPlainDataTypeEnumtype` is doing the exact opposite.
+
+Example: `Fin (2^4+1) × Fin (2^4-1)`
+  
+As Product:
+  The type `Fin (2^4+1)` needs 5 bits.
+  The type `Fin (2^4-1)` needs 4 bits.
+  Thus `Fin (2^4+1) × Fin (2^4-1)` needs 9 bits, thus 2 bytes, as `instPlainDataTypeProd`
+
+As Enumtype:
+  `Fin (2^4+1) × Fin (2^4-1) ≈ Fin (2^8-1)`
+  The type `Fin (2^8-1)` needs 8 bits thus only a single byte as `instPlainDataTypeEnumtype`
+    
+-/
+instance instPlainDataTypeProd [ta : PlainDataType α] [tb : PlainDataType β] : PlainDataType (α×β) where
+  btype :=
+    match ta.btype, tb.btype with
+    | .inl aBitType,  .inl bBitType  => Prod.bitTypeProd aBitType bBitType
+    | .inl aBitType,  .inr bByteType => .inr <| Prod.bitTypeByteTypeProd aBitType bByteType
+    | .inr aByteType, .inl bBitType  => .inr <| Prod.byteTypeBitTypeProd aByteType bBitType 
+    | .inr aByteType, .inr bByteType => .inr <| Prod.byteTypeProd aByteType bByteType 
 
 --------------- Fin n ------------------------------------------------
 ----------------------------------------------------------------------
 
-def BitSize.ofFin (n : Nat) : BitSize :=
-  let bits := Nat.log2 n + (n - 2^(Nat.log2 n) != 0).toUInt64.toNat
-  if bits < 8 then
-    .bits bits.toUInt8 sorry_proof
-  else
-    .bytes ((bits + 7) / 8)
+/-- Number of bits necessary to store `Fin n` -/
+def Fin.bitSize  (n : Nat) : Nat := (Nat.log2 n + (n - 2^(Nat.log2 n) != 0).toUInt64.toNat)
+def Fin.byteSize (n : Nat) : Nat := (Fin.bitSize n + 7) / 8
 
-@[inline]
-opaque Fin.getFromByteArray (n : Nat) [Fact (n≠0)] (d : ByteArray) (i : Nat) (h : (BitSize.ofFin n).InBounds d i ) : Fin n :=
-  match (BitSize.ofFin n) with
-  | .bits m _ => 
-    let ones : UInt8 := 255
-    let mask := ones - (ones <<< m) -- 00000111  for m = 3
-    let perByte := 8/m
-    let inByte  := (i % perByte.toNat).toUInt8
-    let ofByte  := i / perByte.toNat
-    let byte    := d[ofByte]'sorry_proof
-    let val     := mask &&& (byte >>> (m*inByte))
-    ⟨val.toNat, sorry_proof⟩
-  | .bytes m => Id.run do
+def Fin.bitType (n : Nat) (_ : n ≠ 0) (_ : n ≤ 2^8) : BitType (Fin n) where
+  bits := (bitSize n).toUInt8
+  h_size := sorry_proof
+  fromByte b := ⟨b.toNat % n, sorry_proof⟩ --- The modulo here is just in case to remove junk bit values, also we need `n≠0` for consistency
+  toByte   b := b.1.toUInt8
+  fromByte_toByte := sorry_proof
+
+def Fin.byteType (n : Nat) (_ : 2^8 < n) : ByteType (Fin n) where
+  bytes := byteSize n
+  h_size := sorry_proof
+
+  fromByteArray b i h := Id.run do
+    let bytes  := byteSize n
+    let ofByte := i * bytes
+
     let mut val : Nat := 0
-    let ofByte := i * m
-    for j in [0:m] do
-      val := val + ((d[ofByte+j]'sorry_proof).toNat <<< (j*8))
+    for j in [0:bytes] do
+      val := val + ((b[ofByte+j]'sorry_proof).toNat <<< (j*8))
     ⟨val, sorry_proof⟩
 
-@[inline]
-opaque Fin.setInByteArray (n : Nat) (d : ByteArray) (i : Nat) (h : (BitSize.ofFin n).InBounds d i) (val : Fin n) : ByteArray :=
-  match (BitSize.ofFin n) with
-  | .bits m _ => 
-    let perByte := 8/m
-    let inByte  := (i % perByte.toNat).toUInt8
-    let ofByte  := i / perByte.toNat
-    let ones : UInt8 := 255
-    let mask    := ones - ((ones - (ones <<< m)) <<< (inByte*m))  --- 11000111 for m = 3 and inByte = 1
-    let byte    := d[ofByte]'sorry_proof
-    let newByte := (mask &&& byte) + (val.1.toUInt8 <<< (inByte*m))
-    d.set ⟨ofByte, sorry_proof⟩ newByte
-  | .bytes m => Id.run do
-    let mut d := d
-    let ofByte := i * m
-    for j in [0:m] do
-      d := d.set ⟨ofByte+j, sorry_proof⟩ (val.1 >>> (j*8)).toUInt8
-    d
+  toByteArray b i h val := Id.run do
+    let bytes  := byteSize n
+    let ofByte := i * bytes
 
-instance (n) [Fact (n≠0)] : PlainDataType (Fin n) where
-  size := BitSize.ofFin n
-  get := Fin.getFromByteArray n
-  set := Fin.setInByteArray n
+    let mut b := b
+    for j in [0:bytes] do
+      b := b.set ⟨ofByte+j, sorry_proof⟩ (val.1 >>> (j*8)).toUInt8
+    b
+    
+  toByteArray_size := sorry_proof
+  fromByteArray_toByteArray := sorry_proof
+  fromByteArray_toByteArray_other := sorry_proof
 
+instance (n) [f : Fact (n≠0)] : PlainDataType (Fin n) where
+  btype := 
+    if h : n ≤ 2^8 
+    then .inl (Fin.bitType n f.proof h) 
+    else .inr (Fin.byteType n (by simp at h; apply h))
 
 -------------- Enumtype ----------------------------------------------
 ----------------------------------------------------------------------
 
-instance (priority := low) {ι : Type} [Enumtype ι] [Nonempty ι] : PlainDataType ι where
-  size := BitSize.ofFin (numOf ι)
-  get d i h := fromFin (Fin.getFromByteArray (numOf ι) d i h)
-  set d i h val := (Fin.setInByteArray (numOf ι) d i h (toFin val))
+def Enumtype.bitType (α : Type) [Enumtype α] [Nonempty α] (h : numOf α ≤ 2^8) : BitType α where
+  bits := Fin.bitSize (numOf α) |>.toUInt8
+  h_size := sorry_proof
+  fromByte b := fromFin <| (Fin.bitType (numOf α) sorry_proof h).fromByte b
+  toByte a   := (Fin.bitType (numOf α) sorry_proof h).toByte (toFin a)
+  fromByte_toByte := sorry_proof
 
-def Float.getByteData (d : ByteArray) (i : Nat) (_ : (BitSize.bytes 8).InBounds d i) : Float := 
-  let d : FloatArray := cast sorry_proof d
-  d.get ⟨i, sorry_proof⟩
+def Enumtype.byteType (α : Type) [Enumtype α] [Nonempty α] (hn : 2^8 < numOf α ) : ByteType α where
+  bytes := Fin.byteSize (numOf α)
+  h_size := sorry_proof
 
+  fromByteArray b i h := fromFin <| (Fin.byteType (numOf α) hn).fromByteArray b i h
+  toByteArray b i h a := (Fin.byteType (numOf α) hn).toByteArray b i h (toFin a)
+    
+  toByteArray_size := sorry_proof
+  fromByteArray_toByteArray := sorry_proof
+  fromByteArray_toByteArray_other := sorry_proof
+
+
+/-- Enumtype is `PlainDataType` via conversion from/to `Fin n`
+
+**Instance diamond** This instance `instPlainDataTypeProd` is prefered over this one.
+
+This instance makes a diamond together with `instPlainDataTypeProd`. Using this instance is more computationally intensive when writting and reading from `DataArra` but it consumes less memory. The `instPlainDataTypeProd` is doing the exact opposite.
+
+Example: `Fin (2^4+1) × Fin (2^4-1)`
+  
+As Product:
+  The type `Fin (2^4+1)` needs 5 bits.
+  The type `Fin (2^4-1)` needs 4 bits.
+  Thus `Fin (2^4+1) × Fin (2^4-1)` needs 9 bits, thus 2 bytes, as `instPlainDataTypeProd`
+
+As Enumtype:
+  `Fin (2^4+1) × Fin (2^4-1) ≈ Fin (2^8-1)`
+  The type `Fin (2^8-1)` needs 8 bits thus only a single byte as `instPlainDataTypeEnumtype`
+    
+-/
+instance (priority := low) instPlainDataTypeEnumtype  {α : Type} [Enumtype α] [Nonempty α] : PlainDataType α where
+  btype := 
+    if h : (numOf α) ≤ 2^8 
+    then .inl (Enumtype.bitType α h)
+    else .inr (Enumtype.byteType α (by simp at h; apply h))
 
 -------------- Float -------------------------------------------------
 ----------------------------------------------------------------------
 
+def Float.byteType : ByteType Float where
+  bytes := 8
+  h_size := sorry_proof
 
-def Float.setByteData (d : ByteArray) (i : Nat) (_ : (BitSize.bytes 8).InBounds d i) (val : Float) : ByteArray := Id.run do
-  let mut d : FloatArray := cast sorry_proof d
-  d := d.set ⟨i, sorry_proof⟩ val
-  cast sorry_proof d
+  fromByteArray arr i _ := 
+    if i % 8 = 0 then
+      let arr : FloatArray := cast sorry_proof arr
+      arr[i/8]'sorry_proof
+    else
+      panic! "Can't read float from ByteArray from nonaligned position i={i}! i % 8 = {i%8} ≠ 0. Please implement this case in C!"
+  toByteArray arr i _ a :=
+    if i % 8 = 0 then
+      let arr : FloatArray := cast sorry_proof arr
+      cast sorry_proof (arr.set ⟨i/8, sorry_proof⟩ a)
+    else
+      panic! "Can't write float to ByteArray to nonaligned position i={i}! i % 8 = {i%8} ≠ 0. Please implement this case in C!"
+
+  toByteArray_size := sorry_proof
+  fromByteArray_toByteArray := sorry_proof
+  fromByteArray_toByteArray_other := sorry_proof
+
 
 instance : PlainDataType Float where
-  size := .bytes 8
-  get := Float.getByteData
-  set := Float.setByteData
+  btype := .inr Float.byteType
 
+-------------- Real --------------------------------------------------
+----------------------------------------------------------------------
+
+def ℝ.byteType : ByteType ℝ where    
+  bytes := 8
+  h_size := sorry_proof
+
+  fromByteArray b i h := Float.byteType.fromByteArray b i h |>.toReal
+  toByteArray b i h a := Float.byteType.toByteArray b i h a.toFloat
+
+  toByteArray_size := sorry_proof
+  fromByteArray_toByteArray := sorry_proof
+  fromByteArray_toByteArray_other := sorry_proof
+
+
+instance : PlainDataType ℝ where
+  btype := .inr ℝ.byteType
