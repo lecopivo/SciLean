fb09f20fa28b56abb8cf747f24dc7844147fe94f
lecopivo <skrivantomas@seznam.cz>
2022-10-06 16:21:56 +0200

    ArrayN is inhabited

--- SciLean/Data/ArrayN.lean
+++ SciLean/Data/ArrayN.lean
@@ -10,4 +10,10 @@ structure ArrayN (α : Type) (n : Nat) where
 instance : GetElem (ArrayN α n) (Fin n) α (λ _ _ => True) where
   getElem arr i _ := arr.data.get (arr.h_size ▸ i)
 
+instance [Inhabited α] : Inhabited (ArrayN α n) :=
+  ⟨{
+    data := .mkArray n default
+    h_size := by simp
+  }⟩
+
 end SciLean
