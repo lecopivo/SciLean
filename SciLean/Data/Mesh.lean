c6b7f4ead19f6da4070eea88eecfe05f2c566f9b
lecopivo <skrivantomas@seznam.cz>
2022-10-06 16:25:29 +0200

    Main Mesh.lean file

---
+++ SciLean/Data/Mesh.lean
@@ -0,0 +1,6 @@
+import SciLean.Data.Mesh.Prism
+import SciLean.Data.Mesh.PrismaticSet
+import SciLean.Data.Mesh.PrismaticMesh
+import SciLean.Data.Mesh.Line
+import SciLean.Data.Mesh.Segment
+import SciLean.Data.Mesh.Circle
