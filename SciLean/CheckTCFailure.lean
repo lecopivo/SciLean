ca49533eca93022ba5c391e3503cf32bca4ce40f
lecopivo <skrivantomas@seznam.cz>
2022-10-31 11:36:21 +0100

    command #check_tc_failure can now detect max hearthbeat

--- SciLean/CheckTCFailure.lean
+++ SciLean/CheckTCFailure.lean
@@ -4,6 +4,9 @@ open Lean Elab Command Term Meta
 #check synthInstance
 #check MetaM
 
+/-- Checks if typeclass fails to be synthesized. The failure has to be proper i.e. max hearthbeat is not reached thus all options are exhausted. 
+
+TODO: Make more robust! The command currently just checks the error message. Also check for reaching max size. -/
 syntax (name := check_tc_failure) "#check_tc_failure" term : command
 
 def Lean.MessageData.type : MessageData → String
@@ -20,38 +23,36 @@ def Lean.MessageData.type : MessageData → String
   | _ => "unknown"
 
 
-@[commandElab check_tc_failure]
+@[command_elab check_tc_failure]
 def checkTCFailureImpl : CommandElab := fun stx => do
   try 
     let _ ← liftTermElabM (withoutErrToSorry (elabTerm stx[1] none))
     logError "Unexpected success!"
   catch e =>
-    logInfo "FAILURE"
-    logInfo s!"{← e.toMessageData.toString}"
-    logInfo s!"{e.toMessageData.type}"
+    let msg ← e.toMessageData.toString
+    let msg' := msg |>.dropWhile (· != '\n') |>.drop 1 |>.dropWhile (· != '\n') |>.drop 1
+    if msg'.startsWith "(deterministic) timeout at 'typeclass', maximum number of heartbeats" then
+      logError s!"Typeclass synthesis failed by reaching the maximum number of heartbeats!"
+    else 
+      logInfo s!"Proper typeclass failure!\n{msg}"
 
 class C (n : Nat)
 instance : C 0 := ⟨⟩
 def funC (n : Nat) [C n] := n
 
 -- This behaves as expected
-#check_tc_failure funC 0
+-- #check_tc_failure funC 0 -- fails as expected
 #check_tc_failure funC 1
 
 class D (n : Nat)
-instance {n} [D (n+1)] : D n := ⟨⟩
+instance {n} [D n] : D (n+1) := ⟨⟩
 def funD (n : Nat) [D n] := n
 
 -- I want this to fail as it reaches max heartbeats
-#check_tc_failure (funD 0)
+#check_tc_failure (funD 100)
 
-class E (n m : Nat)
-instance [E n m] [E n (m+1)] : E (n+1) m := ⟨⟩
-def funE (n m : Nat) [E n m] := n+m
+-- set_option synthInstance.maxHeartbeats 50 in
+-- #check_tc_failure (funD 100) -- correctly fails because reaching max hearthbeat
 
--- This should fail because we are hitting maxSize 
-#check_tc_failure funE 7 0
-
--- This should succeed as we have searched through everything
-set_option synthInstance.maxSize 300 in
-#check_tc_failure funE 7 0
+set_option synthInstance.maxSize 1000 in
+#check_tc_failure (funD 100)
