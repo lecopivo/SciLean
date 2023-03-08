ec148d54cab0cd504a8a5ad0e4102e1ba429a7de
lecopivo <skrivantomas@seznam.cz>
2023-03-08 17:26:39 -0500

    properties of some functions on PowType

---
+++ SciLean/Data/ArrayType/PowTypeProperties.lean
@@ -0,0 +1,45 @@
+import SciLean.Data.ArrayType.PowType
+import SciLean.Data.ArrayType.Properties
+
+
+namespace SciLean
+
+variable {X I} {T : outParam Type} [Enumtype I] [PowType T I X] -- [Inhabited X]
+
+--------------------------------------------------------------------------------
+-- introElem 
+--------------------------------------------------------------------------------
+
+function_properties introPowElem [Vec X] (f : I → X) : X^I
+argument f
+  isLin := by unfold introPowElem; apply IsLinN.mk,
+  isSmooth,
+  abbrev ∂ 𝒯 := introPowElem df by symdiff
+
+
+-- set_option pp.all true in
+set_option trace.Meta.Tactic.simp.discharge true in
+set_option trace.Meta.Tactic.simp.rewrite true in
+set_option synthInstance.maxSize 2000 in
+function_properties introPowElem [SemiHilbert X] (f : I → X) : X^I
+argument f
+  hasAdjoint := by unfold introPowElem; apply HasAdjointN.mk,
+  abbrev † := λ idx => f'[idx] by unfold introPowElem; rw[introElem.arg_f.adjoint_simp], -- TODO: Figure out why I have to manually invoke `introElem.arg_f.adjoint_simp`
+  hasAdjDiff
+  -- This causes some issues, the `dom` in `getElem` fails to be infered automatically  
+  -- and even when provided explicitly it casauses mayham.
+  -- Therefore we state it separetely
+  -- abbrev ∂† ℛ := λ idx => df'[idx] by unfold adjointDifferential; symdiff; symdiff; done
+
+-- TODO: 
+@[diff]
+theorem introPowElem.arg_f.adjDiff_simp [SemiHilbert X]
+  : ∂† (λ (f : I → X) => introPowElem f)
+    =
+    λ f df' => (λ idx => df'[idx]) := sorry_proof
+
+@[diff]
+theorem introPowElem.arg_f.revDiff_simp [SemiHilbert X]
+  : ℛ (λ (f : I → X) => introPowElem f)
+    =
+    λ f => (introPowElem f, λ df' => (λ idx => df'[idx])) := by unfold reverseDifferential; symdiff
