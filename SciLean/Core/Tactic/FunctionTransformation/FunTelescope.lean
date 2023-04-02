accb4eb9ddd7f41ffec48cc2b6e0c052c440201c
lecopivo <skrivantomas@seznam.cz>
2023-04-03 01:35:28 +0200

    getting funcion, arguments, applying arguments, beta and eta reductions

--- SciLean/Core/Tactic/FunctionTransformation/FunTelescope.lean
+++ SciLean/Core/Tactic/FunctionTransformation/FunTelescope.lean
@@ -10,13 +10,12 @@ namespace SciLean
 
 open Lean Meta
 
-
 variable [MonadControlT MetaM n] [Monad n]
 
 inductive FunType where
   | normal                    -- normal function
   | map (funSpaceName : Name) -- special kind of map like linear, smooth, continuous etc.
-deriving Repr, BEq
+deriving Repr, BEq, Inhabited
 
 instance : ToString FunType := ⟨λ t => toString (repr t)⟩
 
@@ -67,23 +66,39 @@ def isFunLambda? (e : Expr) : Option (FunType × Name × Expr × Expr × BinderI
     return (.map ``PowType, n, d, b, bi)
   | _ => none
 
-def isApp? (e : Expr) : Option (FunType × Expr × Expr) :=
+def isFunApp? (e : Expr) : Option (FunType × Expr × Expr) :=
   match e with
-  | .app (.lam ..) x => 
-    return (.normal, e.getAppFn, x)
   | mkApp6 (.const ``SmoothMap.val _) _ _ _ _ f x => 
     return (.map ``SmoothMap, f ,x)
   | mkApp6 (.const ``LinMap.val _) _ _ _ _ f x => 
     return (.map ``LinMap, f ,x)
   | mkApp8 (.const ``getElem _) _ _ _ _ _ f x _ =>
     return (.map ``PowType, f ,x) -- should we double check that it is indeed PowType ? 
+  | .app f x => 
+    return (.normal, f, x)
   | _ => none
 
-def isFunSpace? (e : Expr) : MetaM (Option (FunType × Expr × Expr)) := 
+partial def getFunAppFn (e : Expr) : Expr := 
+  if let .some (_, f, _) := isFunApp? e then
+    getFunAppFn f
+  else
+    e
+
+-- TODO: implement version that does not create reversed array
+private partial def getFunAppArgsRevAux (e : Expr) (args : Array Expr) : Expr × Array Expr := 
+  if let .some (_, f, x) := isFunApp? e then
+    getFunAppArgsRevAux f (args.push x)
+  else
+    (e, args)
+
+def getFunAppArgs (e : Expr) : Array Expr := 
+  (getFunAppArgsRevAux e #[]).2.reverse
+
+def isFunSpace? (e : Expr) : MetaM (Option FunType) := 
   match e with  
-  | .forallE _ X Y _ => if !Y.hasLooseBVars then return some (.normal, X, Y) else return none
-  | mkApp4 (.const ``SmoothMap _) X Y _ _ => return some (.map ``SmoothMap, X, Y)
-  | mkApp4 (.const ``LinMap _) X Y _ _ => return some (.map ``LinMap, X, Y)
+  | .forallE .. => return some .normal
+  | mkApp4 (.const ``SmoothMap _) _ _ _ _ => return some (.map ``SmoothMap)
+  | mkApp4 (.const ``LinMap _) _ _ _ _ => return some (.map ``LinMap)
   | e => do
     let X? ← Meta.mkFreshExprMVar (mkSort levelOne)
     let Y? ← Meta.mkFreshExprMVar (mkSort levelOne)
@@ -94,10 +109,10 @@ def isFunSpace? (e : Expr) : MetaM (Option (FunType × Expr × Expr)) :=
       | return none
 
     -- -- make sure that it is indeed PowType
-    -- let .some _ ← trySynthInstance (mkApp3 (.const ``PowType []) e X? Y?)
-    --   | return none
+    -- if ¬(← isDefEq e (← mkAppOptM ``PowTypeCarrier #[X?, Y?, none, none])) then
+    --   return none
 
-    return some (.map ``PowType, X?, Y?)
+    return some (.map ``PowType)
 
 def mkFunSpace (funType : FunType) (X Y : Expr) : MetaM Expr :=
   match funType with
@@ -107,20 +122,34 @@ def mkFunSpace (funType : FunType) (X Y : Expr) : MetaM Expr :=
   | .map ``PowType => mkAppM ``PowTypeCarrier #[X,Y]
   | .map n => throwError "Error in `mkFunSpace`, unrecognized function type `{n}`"
 
-def mkFunAppM (f x : Expr) : MetaM Expr := do
+def mkFunApp1M (f x : Expr) : MetaM Expr := do
   let F ← inferType f
-  let .some (funType, X, Y) ← isFunSpace? F
+  let .some funType ← isFunSpace? F
     | throwError "Error in `mkFunAppM`: `{← ppExpr f}` is not a function!"
   match funType with
   | .normal => return .app f x
   | .map ``SmoothMap => mkAppM ``SmoothMap.val #[f, x]
   | .map ``LinMap => mkAppM ``LinMap.val #[f, x]
-  | .map ``PowType => mkAppOptM ``getElem #[none, none, none, none, none, f, x, some (.const ``True.intro [])]
+  | .map ``PowType => do
+    let fx ← mkAppM ``getElem #[f, x]
+    mkAppM' fx #[(.const ``True.intro [])]
   | .map n => throwError "Error in `mkFunSpace`, unrecognized function type `{n}`"
 
--- This does not work as expecteed
+def mkFunAppM (f : Expr) (xs : Array Expr) : MetaM Expr := 
+  xs.foldlM (init := f) λ f x => mkFunApp1M f x
+  -- let F ← inferType f
+  -- let .some (funType, _, _) ← isFunSpace? F
+  --   | throwError "Error in `mkFunAppM`: `{← ppExpr f}` is not a function!"
+  -- match funType with
+  -- | .normal => return .app f x
+  -- | .map ``SmoothMap => mkAppM ``SmoothMap.val #[f, x]
+  -- | .map ``LinMap => mkAppM ``LinMap.val #[f, x]
+  -- | .map ``PowType => mkAppOptM ``getElem #[none, none, none, none, none, f, x, some (.const ``True.intro [])]
+  -- | .map n => throwError "Error in `mkFunSpace`, unrecognized function type `{n}`"
+
+-- This does not work at all!
 def funHeadBeta1 (e : Expr) : Expr := 
-  if let .some (funType, f, x) := isApp? e then
+  if let .some (funType, f, x) := isFunApp? e then
     if let .some (funType', _, _, b, _) := isFunLambda? f then
       if funType == funType' then
         b.instantiate1 x
@@ -131,41 +160,80 @@ def funHeadBeta1 (e : Expr) : Expr :=
   else
     e
 
-private partial def funTelescopeImp (e : Expr) (k : Array (Expr × FunType) → Expr → MetaM α) : MetaM α := do
-  process (← getLCtx) #[] #[] 0 e
-where
-  process (lctx : LocalContext) (fvars : Array Expr) (funTypes : Array FunType) (j : Nat) (e : Expr) : MetaM α := do
-    if let .some (funType, n, d, b, bi) := isFunLambda? e then
-      let d := d.instantiateRevRange j fvars.size fvars
-      let fvarId ← mkFreshFVarId
-      let lctx := lctx.mkLocalDecl fvarId n d bi
-      let fvar := mkFVar fvarId
-      process lctx (fvars.push fvar) (funTypes.push funType) j b
+-- There is probably a better implementation
+def funHeadBeta (e : Expr) : MetaM Expr := 
+  let f := getFunAppFn e
+  let args := getFunAppArgs e
+  args.foldlM (init := f) λ f arg => do
+    if let .some (_, _, _, b, _) := isFunLambda? f then
+      pure (b.instantiate1 arg)
     else
-      let e := e.instantiateRevRange j fvars.size fvars
-      withReader (fun ctx => { ctx with lctx := lctx }) do
-        -- withNewLocalInstancesImp fvars j do -- no clue what this does
-          k (fvars.zip funTypes) e
+      mkFunApp1M f arg
+
+
+private partial def funTelescopeImp (e : Expr) (maxArg? : Option Nat) (k : Array (Expr × FunType) → Expr → MetaM α) : MetaM α := do
+  process (← getLCtx) #[] #[] false e
+where
+  process (lctx : LocalContext) (fvars : Array Expr) (funTypes : Array FunType) (stop : Bool) (e : Expr) : MetaM α := do
+    if let .some (funType, n, d, b, bi) := isFunLambda? e then
+      if ¬stop then 
+        let stop := 
+          match maxArg? with
+          | some maxArg => fvars.size + 1 = maxArg 
+          | none => false
+        let d := d.instantiateRevRange 0 fvars.size fvars
+        let fvarId ← mkFreshFVarId
+        let lctx := lctx.mkLocalDecl fvarId n d bi
+        let fvar := mkFVar fvarId
+        return ← (process lctx (fvars.push fvar) (funTypes.push funType) stop b)
+
+    let e := e.instantiateRevRange 0 fvars.size fvars
+    withReader (fun ctx => { ctx with lctx := lctx }) do
+      -- withNewLocalInstancesImp fvars j do -- no clue what this does
+        k (fvars.zip funTypes) e
 
 def funTelescope (e : Expr) (k : Array (Expr × FunType) → Expr → n α) : n α := 
-  map2MetaM (fun k => funTelescopeImp e k) k
+  map2MetaM (fun k => funTelescopeImp e none k) k
 
-#check mkLambdaFVars
+/-- Same as `funTelescope` but you can limit the number of consumed arguments -/
+def funTelescope' (e : Expr) (maxArg : Nat) (k : Array (Expr × FunType) → Expr → n α) : n α := 
+  map2MetaM (fun k => funTelescopeImp e (some maxArg) k) k
 
 def mkFunFVars (xs : Array (Expr × FunType)) (e : Expr) : MetaM Expr := do
   xs.foldrM (init := e) λ (x, funType) e => do pure (← funType.mkFun (← mkLambdaFVars #[x] e))
 
+-- There is probably a better implementation
+def funEta (e : Expr) : MetaM Expr := do
+  funTelescope e λ xs b => do
+    let mut b := b
+    let mut j := xs.size
+    for _ in [0:xs.size] do
+      let (x,funType) := xs[j-1]!
+      if let .some (funType', f, x') := isFunApp? b then
+        if (funType == funType') && (x == x') then
+          j := j - 1
+          b := f
+      else
+        break
+    mkFunFVars xs[0:j] b
+
+#check Smooth.differential
+
+def bar : (ℝ ⟿ ℝ) ⟿ ℝ ⟿ ℝ ⊸ ℝ := sorry
+#check (λ (f : ℝ ⟿ ℝ) (x : ℝ) ⟿ bar f x)
 
 open Qq
 #eval show MetaM Unit from do
   let f := q(λ (n : Nat) => λ (x : ℝ) ⟿ λ (z : ℝ) ⊸ z)
   let f := q(λ (n : Nat) => λ (x : ℝ) ⟿ ⊞ (i : Fin n), (x:ℝ))
+
   
   IO.println s!"original function:  {← ppExpr f}"
   
-  funTelescope f λ xs b => do
+  funTelescope' f 2 λ xs b => do
     let f' ← mkFunFVars xs b
     IO.println s!"arguments: {← xs.mapM λ (x,_) => ppExpr x}"
+    IO.println s!"body: {← ppExpr b}"
     IO.println s!"recovered function: {← ppExpr f'}"
 
   IO.println s!"`ℝ → ℝ` is function space: {(← isFunSpace? q(ℝ→ℝ)).isSome}"
@@ -175,12 +243,34 @@ open Qq
   IO.println s!"`Vec3 ℝ` is function space: {(← isFunSpace? q(Vec3 ℝ)).isSome}"
   IO.println s!"`Array ℝ` is function space: {(← isFunSpace? q(Array ℝ)).isSome}"
 
+  let n := q(10 : Nat)
+  let x := q(42 : ℝ)
+  let i := q(5 : Fin 10)
+
+  let fnxi ← mkFunAppM f #[n,x,i]
+  let fnxi' ← funHeadBeta fnxi
+
+  IO.println s!"test application: {← ppExpr fnxi}"
+  IO.println s!"beta red: {← ppExpr fnxi'}"
+  let f'   := getFunAppFn fnxi
+  let nxi' := getFunAppArgs fnxi
+  IO.println s!"Function of application: {← ppExpr f'}"
+  IO.println s!"Args of application: {← (nxi'.mapM ppExpr)}"
+
+  let fst := q(λ xy : Nat × Nat => xy.fst)
+  let add := q(λ (x y : ℝ) ⟿ (λ x y ⟿ x + y) x y)
+  IO.println s!"eta reduced fst: {← ppExpr (← funEta fst)}"
+  IO.println s!"eta reduced add: {← ppExpr (← funEta add)}"
+
   let f := q(λ (x : ℝ) ⟿ x + x)
   let x : Q(ℝ) := q(1)
 
-  IO.println s!"{← ppExpr (← mkFunAppM f x)}"
-  IO.println s!"{← ppExpr (funHeadBeta1 (← (mkFunAppM f x)))}"
+  IO.println s!"{← ppExpr (← mkFunApp1M f x)}"
+  IO.println s!"{← ppExpr (funHeadBeta1 (← (mkFunApp1M f x)))}"
 
-set_option pp.all true in
-#check ArrayType (ℝ^{3}) (Fin 3) ℝ
+-- X → X
+-- X ⟿ X
+-- X ⊸ X
+
+-- (I → X ⟿ Y) → (I → X) → Y^I
 
