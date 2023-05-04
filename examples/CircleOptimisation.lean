551b0106330c9382a727504bbf803d13a71edea2
Siddharth Bhat <siddu.druid@gmail.com>
2023-05-05 00:14:43 +0100

    feat: add working circle optimisation
    Huzzah:
    
    [nix-shell:~/papers/scilean/SciLean]$ ./build/bin/CircleOptimisation
    0 loss[1.000000]
    1 loss[0.959216]
    2 loss[0.916055]
    3 loss[0.870518]
    4 loss[0.822627]
    5 loss[0.772416]
    6 loss[0.719944]
    7 loss[0.665284]
    8 loss[0.608534]
    9 loss[0.549808]
    10 loss[0.489243]
    11 loss[0.426997]
    12 loss[0.363243]
    13 loss[0.298176]
    14 loss[0.232005]
    15 loss[0.164953]
    16 loss[0.097254]
    17 loss[0.029152]
    18 loss[-0.039103]
    19 loss[-0.107261]

--- examples/CircleOptimisation.lean
+++ examples/CircleOptimisation.lean
@@ -11,6 +11,12 @@ open Lean
 open SciLean
 
 
+instance : HDiv (ℝ ^{n}) ℝ (ℝ^{n}) where
+  hDiv v r := ⊞ i, v[i] / r
+
+instance : HMul ℝ (ℝ ^{n}) (ℝ^{n}) where
+  hMul r v := ⊞ i, v[i] * r
+
 
 /-- choose a particular A -/
 -- the function (x, y) -> (x + y, x - y)
@@ -42,23 +48,31 @@ argument x
     -- ⊢ ⟪dx, A x⟫ + ⟪x, A dx⟫ = 2 * ⟪dx, A x⟫
     -- how do I prove this? The natural proof for me is to do this componentwise
     sorry
-  }
+  },
+  abbrev ∂† by { unfold gradient; unfold loss; fun_trans; simp }
 
 noncomputable def grad_loss (x : ℝ^{2}) : ℝ^{2} := ∇ loss x
 
+def grad_loss' (x : ℝ^{2}) : ℝ^{2} :=
+  (∇ loss x) rewrite_by {
+    unfold gradient; unfold loss; fun_trans
+}
+
 
 structure TrainLog where
+  iteration : Nat
   point : ℝ^{2} -- current point on the circle
   loss_at_point : ℝ -- loss value at current point
   gradient_ambient : ℝ^{2} -- gradient at current point
   gradient_circle : ℝ^{2} -- gradient at current point
 
 
-instance : HDiv (ℝ ^{n}) ℝ (ℝ^{n}) where
-  hDiv v r := ⊞ i, v[i] / r
+instance : ToString TrainLog where
+  toString log :=
+   -- TODO: need ToString instances to show.
+   -- s!"p = {log.point} ; loss = {log.loss_at_point.val}; ∇ = {log.gradient_circle}"
+   s!"{log.iteration} loss[{log.loss_at_point.val}]"
 
-instance : HMul ℝ (ℝ ^{n}) (ℝ^{n}) where
-  hMul r v := ⊞ i, v[i] * r
 
 -- normalize a vector.
 def normalize (p : ℝ^{2}) : ℝ^{2} := p / ‖p‖
@@ -78,27 +92,36 @@ def circle_tangent_space_project (p : ℝ^{2}) (vec : ℝ^{2}) : ℝ^{2} :=
   vec - project (direction := p) vec
 
 
-noncomputable def TrainLog.calc (p : ℝ^{2}) : TrainLog where
+def TrainLog.calc (i : Nat) (p : ℝ^{2}) : TrainLog where
+  iteration := i
   point := p
   loss_at_point := loss p
-  gradient_ambient := grad_loss p
-  gradient_circle := circle_tangent_space_project (p := p) (grad_loss p)
+  gradient_ambient := grad_loss' p
+  gradient_circle := circle_tangent_space_project (p := p) (grad_loss' p)
 
+structure HyperParams where
+  learningRate : ℝ := 0.01
 
-def learning_rate : ℝ := 0.01
+def HyperParams.default : HyperParams := {}
+instance : Inhabited HyperParams where default := {}
 
-noncomputable def TrainLog.nextPoint (step : TrainLog) : ℝ^{2} :=
-  circle_project <| step.point - learning_rate * step.gradient_circle
+def TrainLog.nextPoint (hp : HyperParams) (step : TrainLog) : ℝ^{2} :=
+  circle_project <| step.point - hp.learningRate * step.gradient_circle
 
-/-
-TODO: how do I now compute with the `noncomputable` grad_loss?
--/
-noncomputable def plot_loss (init: ℝ^{2}) (nsteps : ℕ) :
+def plot_loss (init: ℝ^{2}) (nsteps : ℕ)
+  (hp : HyperParams := HyperParams.default) :
   Array TrainLog := Id.run do
-    let mut cur : ℝ^{2} := init
+    let mut cur : ℝ^{2} := circle_project init
     let mut out := #[]
-    for _ in List.range nsteps do
-      let step := TrainLog.calc cur
+    for i in List.range nsteps do
+      let step := TrainLog.calc i cur
       out := out.push <| step
-      cur := step.nextPoint
+      cur := step.nextPoint hp
     return out
+
+def main : IO Unit := do
+  let init : ℝ^{2} :=
+    ⊞ i, if i = 0 then 1 else 0
+  let losses := plot_loss init 20
+  for loss in losses do
+    IO.println s!"{loss}"
