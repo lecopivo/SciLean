c72c07706f6604cbaae78b3e86f0bd26950c1218
lecopivo <skrivantomas@seznam.cz>
2023-05-04 14:39:57 -0400

    new command #profile_file that profiles a file and displays result with speedscope

---
+++ SciLean/Profile.lean
@@ -0,0 +1,58 @@
+import Lean
+
+open Lean System IO 
+
+def profileFile (file : FilePath) (flame : FilePath := "/home/tskrivan/Documents/Flame/build/bin/flame") : IO Unit := do
+
+  let compile_output ← IO.Process.output {
+    cmd := "lake"
+    args := #["env", "lean", "-D", "trace.profiler=true", "-D", "pp.oneline=true", file.toString]
+  }
+
+  if compile_output.exitCode != 0 then
+    throw (IO.Error.userError s!"Error: Failed to compile {file}\n\n{compile_output.stderr}")
+
+  let flame_run ← Process.spawn {
+    stdin := .piped
+    stdout := .piped
+    stderr := .piped
+    cmd := flame.toString
+    args := #[compile_output.stdout]
+  }
+  let (stdin, child) ← flame_run.takeStdin
+  stdin.putStr compile_output.stdout
+  stdin.flush
+  let stdout ← IO.asTask child.stdout.readToEnd Task.Priority.dedicated
+  let stderr ← child.stderr.readToEnd
+  let exitCode ← child.wait
+  let stdout ← IO.ofExcept stdout.get
+
+  if exitCode != 0 then
+    throw (IO.Error.userError s!"Error: Failed to run flame\n\n{stderr}")
+
+  let speedscope_run ← Process.spawn {
+    stdin := .piped
+    stdout := .piped
+    stderr := .piped
+    cmd := "speedscope"
+    args := #["-"]
+  }
+  let (stdin, child) ← speedscope_run.takeStdin
+  stdin.putStr stdout
+  stdin.flush
+  let stdout ← IO.asTask child.stdout.readToEnd Task.Priority.dedicated
+  let stderr ← child.stderr.readToEnd
+  let exitCode ← child.wait
+  let stdout ← IO.ofExcept stdout.get
+
+  if exitCode != 0 then
+    throw (IO.Error.userError s!"Error: Failed to run speedscope\n\n{stderr}")
+
+
+
+elab " #profile_file " path:ident : command => do
+
+  let file := (← IO.currentDir) / (path.getId.toString.replace "." FilePath.pathSeparator.toString ++ ".lean")
+  
+  profileFile file
+
