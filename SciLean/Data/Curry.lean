21e40bce4d0cb588058529eceef090ca5d8f414d
lecopivo <skrivantomas@seznam.cz>
2023-08-22 10:47:50 -0400

    fix universes for Curry, Uncurry

--- SciLean/Data/Curry.lean
+++ SciLean/Data/Curry.lean
@@ -4,21 +4,21 @@ namespace SciLean
 
 --------------------------------------------------------------------------------
 
-class UncurryN (n : Nat) (F : Type) (Xs : outParam Type) (Y : outParam Type) where
+class UncurryN (n : Nat) (F : Sort _) (Xs : outParam (Sort _)) (Y : outParam (Sort _)) where
   uncurry : F → Xs → Y
 
 attribute [reducible] UncurryN.uncurry
 
 @[reducible]
-instance (priority := low) {X Y : Type} : UncurryN 1 (X→Y) X Y where
+instance (priority := low) {X Y : Sort _} : UncurryN 1 (X→Y) X Y where
   uncurry := λ (f : X → Y) (x : X) => f x
 
 @[reducible]
-instance {X Y : Type} {Xs' Y' : outParam Type} [c : UncurryN n Y Xs' Y']
+instance {X Y : Sort _} {Xs' Y' : outParam (Sort _)} [c : UncurryN n Y Xs' Y']
   : UncurryN (n+1) (X→Y) (X×Xs') Y' where
   uncurry := λ (f : X → Y) ((x,xs) : X×Xs') => c.uncurry (n:=n) (f x) xs
 
-abbrev uncurryN {F : Type} {Xs Y : outParam Type} 
+abbrev uncurryN {F : Sort _} {Xs Y : outParam (Sort _)} 
   (n : Nat) (f : F) [UncurryN n F Xs Y] 
   := UncurryN.uncurry (n:=n) f
 
@@ -45,7 +45,7 @@ abbrev uncurryAll {F : Type} {Xs Y : outParam Type}
 
 --------------------------------------------------------------------------------
 
-class CurryN (n : Nat) (Xs : Type) (Y : Type) (F : outParam Type) where
+class CurryN (n : Nat) (Xs : Sort _) (Y : Sort _) (F : outParam (Sort _)) where
   curry : (Xs → Y) → F
 
 attribute [reducible] CurryN.curry
@@ -55,17 +55,17 @@ instance (priority := low) : CurryN 1 X Y (X→Y) where
   curry := λ (f : X → Y) => f
 
 @[reducible]
-instance {X Xs Y : Type} {F : outParam Type} [c : outParam $ CurryN n Xs Y F] 
+instance {X Xs Y : Sort _} {F : outParam (Sort _)} [c : outParam $ CurryN n Xs Y F] 
   : CurryN (n+1) (X×Xs) Y (X→F) where
   curry := λ (f : X×Xs → Y) => (λ (x : X) => c.curry (n:=n) (λ y => f (x,y)))
 
-abbrev curryN {Xs Y : outParam $ Type} {F : outParam Type} 
+abbrev curryN {Xs Y : outParam $ Sort _} {F : outParam (Sort _)} 
   (n : Nat) (f : Xs → Y) [outParam $ CurryN n Xs Y F] := CurryN.curry n f
 
 ---
 
 
-class CurryAll (n : Nat) (Xs : Type) (Y : Type) (F : outParam Type) where
+class CurryAll (n : Nat) (Xs : Sort _) (Y : Sort _) (F : outParam (Sort _)) where
   curry : (Xs → Y) → F
 
 attribute [reducible] CurryAll.curry
@@ -75,11 +75,11 @@ instance (priority := low) : CurryAll 1 X Y (X→Y) where
   curry := λ (f : X → Y) => f
 
 @[reducible]
-instance {X Xs Y : Type} {F : outParam Type} [c : outParam $ CurryAll n Xs Y F] 
+instance {X Xs Y : Sort _} {F : outParam (Sort _)} [c : outParam $ CurryAll n Xs Y F] 
   : CurryAll (n+1) (X×Xs) Y (X→F) where
   curry := λ (f : X×Xs → Y) => (λ (x : X) => c.curry (n:=n) (λ y => f (x,y)))
 
-abbrev curryAll {Xs Y : outParam $ Type} {F : outParam Type} 
+abbrev curryAll {Xs Y : outParam $ Sort _} {F : outParam (Sort _)} 
   (n : Nat) (f : Xs → Y) [outParam $ CurryAll n Xs Y F] := CurryAll.curry n f
 
 
