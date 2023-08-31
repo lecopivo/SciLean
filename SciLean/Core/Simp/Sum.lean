746583b34c22dabbdaacd8d3274ac550057da5cb
lecopivo <skrivantomas@seznam.cz>
2023-08-31 09:31:57 -0400

    simp lemmas for mixing sums and if statements

---
+++ SciLean/Core/Simp/Sum.lean
@@ -0,0 +1,26 @@
+import SciLean.Data.EnumType
+
+namespace SciLean
+
+variable {ι} [EnumType ι]
+
+theorem sum_if {β : Type _} [AddCommMonoid β] (f : ι → β)  (j : ι)
+  : (∑ i, if i = j then f i else 0)
+    =
+    f j
+  := sorry_proof
+
+@[simp]
+theorem sum_if' {β : Type _} [AddCommMonoid β] (f : ι → β)  (j : ι)
+  : (∑ i, if j = i then f i else 0)
+    =
+    f j
+  := sorry_proof
+
+@[simp]
+theorem sum_lambda_swap {α β : Type _} [AddCommMonoid β] (f : ι → α → β) 
+  : ∑ i, (fun a => f i a)
+    =
+    fun a => ∑ i, f i a
+  := sorry_proof
+
