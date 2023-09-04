d1909cc3a809b0585cf3b07127360d00994021af
lecopivo <skrivantomas@seznam.cz>
2023-09-04 17:25:36 -0400

    doodling around with dense layers

---
+++ SciLean/Doodle.lean
@@ -0,0 +1,196 @@
+import SciLean
+import SciLean.Util.Profile
+import SciLean.Tactic.LetNormalize
+import SciLean.Util.RewriteBy
+
+open SciLean
+
+variable 
+  {K : Type} [RealScalar K]
+  {X : Type} [SemiInnerProductSpace K X]
+  {Y : Type} [SemiInnerProductSpace K Y]
+  {Z : Type} [SemiInnerProductSpace K Z]
+  {W : Type} [SemiInnerProductSpace K W]
+  {ι : Type} [EnumType ι]
+  {E : ι → Type _} [∀ i, SemiInnerProductSpace K (E i)]
+
+set_default_scalar K 
+
+theorem revCDeriv.factor_through_app [Index I] [Nonempty I] [Index J]
+  (f : I → Y → Z) (g : X → J → Y) (h : I → J)
+  (hf : ∀ i, HasAdjDiff K (f i)) (hg : HasAdjDiff K g) 
+  (hg' : ∀ j, HasAdjDiff K (g · j)) (hh : Function.Bijective h)
+  : (revCDeriv K fun x i => f i ((g x) (h i)))
+    = 
+    let df' := fun i => gradient K (fun y => f i y)
+    let ih := Function.invFun h
+    fun x => 
+      let jydg := revCDeriv K g x
+      (fun i => f i (jydg.1 (h i)), 
+       fun dz => 
+         let rhs := fun j => df' (ih j) (jydg.1 j) (dz (ih j))
+         jydg.2 rhs) := 
+by
+  conv => 
+    lhs
+    autodiff
+    simp [revCDeriv]
+  funext x
+  simp[revCDeriv,gradient]
+  funext dz
+  set_option trace.Meta.Tactic.simp.discharge true in
+  ftrans only
+  sorry_proof -- it should be possible to push the sum inside and be done
+
+
+theorem revCDeriv.factor_through_getElem 
+  {I J JY} [ArrayType JY J Y] [Index I] [Nonempty I] [Index J] 
+  (f : I → Y → Z) (g : X → JY) (h : I → J)
+  (hf : ∀ i, HasAdjDiff K (f i)) (hg : HasAdjDiff K g) (hg' : ∀ i, HasAdjDiff K (fun x => (g x)[i])) (hh : Function.Bijective h)
+  : (revCDeriv K fun x i => f i ((g x)[h i]))
+    = 
+    let df' := fun i => gradient K (fun y => f i y)
+    let ih := Function.invFun h
+    fun x => 
+      let jydg := revCDeriv K g x
+      (fun i => f i (jydg.1[h i]), 
+       fun dz => 
+         let rhs := introElem (Cont:=JY) (fun j => df' (ih j) (jydg.1[j]) (dz (ih j)))
+         jydg.2 rhs) := 
+by
+  conv => 
+    lhs
+    autodiff
+    simp [revCDeriv]
+  funext x
+  simp[revCDeriv,gradient]
+  funext dz
+  set_option trace.Meta.Tactic.simp.discharge true in
+  ftrans only
+  sorry_proof -- it should be possible to push the sum inside and be done
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+variable {ι κ : Type} [Index κ] [Index ι] [Nonempty ι] [Nonempty κ] [PlainDataType K] 
+
+example (x : W → K ^ ι) (hx : ∀ i, HasAdjDiff K (fun w => (x w)[i])) (hx : HasAdjDiff K x)
+  : <∂ (fun w i => (x w)[i])
+    =
+    fun w => 
+      let xdx := <∂ (w':=w), x w'
+      (fun i => xdx.1[i], fun dy => let dy' := ⊞ i => dy i; xdx.2 dy') :=
+by 
+  have q := revCDeriv.factor_through_getElem (K:=K) (fun _ y => y) (fun w => x w) (fun i => i) sorry sorry sorry sorry
+  rw[q]
+  conv => lhs; autodiff
+
+
+example 
+  : <∂ (fun (x : K ^ ι) i => x[i])
+    =
+    fun x => 
+      (fun i => x[i], fun dy => ⊞ i => dy i) :=
+by 
+  have q := revCDeriv.factor_through_getElem (K:=K) (fun _ y => y) (fun x : K ^ ι => x) (fun i => i) sorry sorry sorry sorry
+  rw[q]
+  conv => lhs; autodiff
+
+
+example 
+  : <∂ (fun (x : K ^ ι) i => (2:K) * x[i])
+    =
+    fun x => 
+      (fun i => (2:K) * x[i], fun dy => ⊞ i => (2:K) * dy i) :=
+by 
+  have q := revCDeriv.factor_through_getElem (K:=K) (fun _ y => 2 * y) (fun x : K ^ ι => x) (fun i => i) sorry sorry sorry sorry
+  rw[q]
+  conv => lhs; autodiff
+
+
+example 
+  (A : ι → κ → K)
+  : <∂ (fun (x : K ^ κ) i => ∑ j, A i j * x[j])
+    =
+    fun x => 
+      (fun i => ∑ j, A i j * x[j], 
+       fun dy => ⊞ j => ∑ i, A i j * dy i) :=
+by 
+  conv => 
+    lhs
+    conv => 
+      enter [x,2,x]
+      simp only [← sum_lambda_swap]
+    simp (config := {zeta:=false}) only [EnumType.sum.arg_f.revCDeriv_rule _ sorry]
+    simp (config := {zeta:=false}) only [revCDeriv.factor_through_getElem (K:=K) (fun j y i => A i j * y) (fun x : K ^ κ => x) (fun i => i) sorry sorry sorry sorry]
+    autodiff
+    autodiff
+
+
+
+example 
+  (A : ι → κ → K)
+  : (<∂ (x : K ^ κ), ⊞ i => ∑ j, A i j * x[j])
+    =
+    fun x => 
+      (⊞ i => ∑ j, A i j * x[j], 
+       fun dy => ⊞ j => ∑ i, A i j * dy[i]) :=
+by 
+  conv => 
+    lhs
+    conv => 
+      enter [x,2,x]
+      simp only [← ArrayType.sum_introElem]
+    simp (config := {zeta:=false}) only [EnumType.sum.arg_f.revCDeriv_rule _ sorry]
+    simp (config := {zeta:=false}) only [revCDeriv.factor_through_getElem (K:=K) (fun j y => ⊞ i => A i j * y) (fun x : K ^ κ => x) (fun i => i) sorry sorry sorry sorry]
+    autodiff
+    autodiff
+
+#check DataArrayN K (ι×κ)
+
+variable (A : DataArrayN K (ι×κ)) (i : ι) (j : κ)
+
+#check A[(i,j)]
+
+#check Nat
+
+example (x : K ^ κ)
+  : (<∂ (A : DataArrayN K (ι×κ)), ⊞ i => ∑ j, A[(i,j)] * x[j])
+    =
+    fun A : K ^ (ι×κ) => 
+      (⊞ i : ι => ∑ (j : κ), A[(i,j)] * x[j],
+       fun dy => ⊞ ij => dy[ij.1] * x[ij.2]) := 
+       -- fun dy => ⊞ ij => dy[ij.1] * x[ij.2]) := 
+by 
+  conv => 
+    lhs
+    conv => 
+      enter [A,2,A']
+      simp only [← ArrayType.sum_introElem]
+    simp (config := {zeta:=false}) only [EnumType.sum.arg_f.revCDeriv_rule _ sorry]
+    simp (config := {zeta:=false}) only [revCDeriv.factor_through_getElem (K:=K) (fun j y => ⊞ i => A i j * y) (fun x : K ^ κ => x) (fun i => i) sorry sorry sorry sorry]
+    autodiff
+    autodiff
+
+
+  
+
+
+
+
+example [Index ι] [PlainDataType K] (x : W → K ^ ι) (hx : ∀ i, HasAdjDiff K (fun w => (x w)[i]))
+  : <∂ w, ∑ i, (x w)[i]
+    =
+    fun w =>
+      let xdx := <∂ (w':=w), x w'
+      (∑ i, xdx.1[i], fun dy' => xdx.2 (⊞ _ => dy')) := 
+by
+  set_option trace.Meta.Tactic.simp.rewrite true in
+  conv => 
+    lhs
+    simp (config := {zeta := false}) only [EnumType.sum.arg_f.revCDeriv_rule _ sorry]
+  
+
+
+  
--- SciLean/Modules/ML/DenseLayer.lean
+++ SciLean/Modules/ML/DenseLayer.lean
@@ -1,8 +1,10 @@
 import SciLean
-import SciLean.Data.DataArray
-import SciLean.Data.Prod
+-- import SciLean.Data.DataArray
+-- import SciLean.Data.Prod
+-- import SciLean.Util.Profile
 
 namespace SciLean
+
 
 variable 
   -- {R : Type _} [RealScalar R]
@@ -19,23 +21,23 @@ def dense (weights : DataArrayN K (κ×ι)) (bias : DataArrayN K κ) (x : DataAr
 variable {κ}
 
 
-def foo : Float → DataArrayN Float (Idx 10) := sorry
+-- def foo : Float → DataArrayN Float (Idx 10) := sorry
 
--- set_option maxHeartbeats 10000
+-- -- set_option maxHeartbeats 10000
 
--- set_option trace.Meta.isDefEq true in
-open Lean Meta Qq in
-#eval show MetaM Unit from do
+-- -- set_option trace.Meta.isDefEq true in
+-- open Lean Meta Qq in
+-- #eval show MetaM Unit from do
 
-  let X : Q(Type) := q(Float → DataArrayN Float (Idx 10))
-  withLocalDecl `x default X fun x => do
-  let x : Q($X) := x
-  let HX := q(IsDifferentiable Float $x)
-  withLocalDecl `hx default HX fun hx => do
+--   let X : Q(Type) := q(Float → DataArrayN Float (Idx 10))
+--   withLocalDecl `x default X fun x => do
+--   let x : Q($X) := x
+--   let HX := q(IsDifferentiable Float $x)
+--   withLocalDecl `hx default HX fun hx => do
 
-  let H := q(IsDifferentiable Float fun w => ⊞ i => ($x w)[i])
-  let h ← mkFreshExprMVar H 
-  IO.println (← isDefEq hx h)
+--   let H := q(IsDifferentiable Float fun w => ⊞ i => ($x w)[i])
+--   let h ← mkFreshExprMVar H 
+--   IO.println (← isDefEq hx h)
 
 
 
@@ -53,22 +55,48 @@ open Lean Meta Qq in
 --   let h ← mkFreshExprMVar H 
 --   IO.println (← isDefEq hx h)
 
-
-#check Lean.Meta.isDefEq
-
-set_option maxHeartbeats 50000
+-- set_option maxHeartbeats 50000
 
   
--- set_option trace.Meta.Tactic.fprop.step true in
+-- set_option trace.Meta.Tactic.fprop.step true in 
 -- set_option trace.Meta.Tactic.fprop.unify true in
 -- set_option trace.Meta.Tactic.fprop.discharge true in
-set_option profiler true
 
 example 
   (x : Float → DataArrayN Float (Idx 10)) (hx : IsDifferentiable Float x)
   : IsDifferentiable Float (fun w => ⊞ i => (x w)[i]) := 
 by
   fprop
+
+
+variable (x : Float → DataArrayN Float (Idx 10)) (hx : ∀ i : Idx 10, HasAdjDiff Float (fun w => (x w)[i]))
+
+set_default_scalar Float
+
+#check <∂ w, x w
+
+
+example 
+  : (<∂ w, ∑ i, (x w)[i])
+    =
+    fun w => 
+      let xdx := <∂ sorry :=
+by
+  sorry
+
+set_option trace.Meta.Tactic.simp.discharge true in
+set_option trace.Meta.Tactic.simp.rewrite true in
+#check 
+  <∂ w, ∑ i, (x w)[i]
+  rewrite_by
+    simp (config := {zeta := false}) only [SciLean.EnumType.sum.arg_f.revCDeriv_rule _ sorry]
+    simp (config := {zeta := false}) only [SciLean.revCDeriv.pi_rule _ _ sorry]
+    
+
+    simp (config := {zeta := false}) only
+    ftrans only
+
+
 
 @[fprop]
 theorem dense.arg_weightsbiasx.IsDifferentiable_rule 
@@ -81,16 +109,29 @@ by
 
 
 @[ftrans]
-theorem dense.arg_weightsbiasx.asdf
+theorem dense.arg_weightsbiasx.fwdCDeriv_rule
   (weights : W → DataArrayN K (κ×ι)) (bias : W → DataArrayN K κ) (x : W → DataArrayN K ι)
   (hweights : IsDifferentiable K weights) (hbias : IsDifferentiable K bias) (hx : IsDifferentiable K x)
   : (fwdCDeriv K fun w => dense κ (weights w) (bias w) (x w) )
     =
     ((fwdCDeriv K fun w => dense κ (weights w) (bias w) (x w))
-     rewrite_by unfold dense; autodiff) :=
+     rewrite_by unfold dense; autodiff) := 
 by
   unfold dense
-  fprop
+  conv => lhs; autodiff
+
+
+set_option trace.Meta.Tactic.ftrans.step true in
+@[ftrans]
+theorem dense.arg_weightsbiasx.revCDeriv_rule  {W : Type} [SemiInnerProductSpace K W]
+  (weights : W → DataArrayN K (κ×ι)) (bias : W → DataArrayN K κ) (x : W → DataArrayN K ι)
+  (hweights : HasAdjDiff K weights) (hbias : HasAdjDiff K bias) (hx : HasAdjDiff K x)
+  : (revCDeriv K fun w => dense κ (weights w) (bias w) (x w) )
+    =
+    ((revCDeriv K fun w => dense κ (weights w) (bias w) (x w))
+     rewrite_by unfold dense; autodiff; autodiff) := 
+by
+  sorry
 
 
 #exit
