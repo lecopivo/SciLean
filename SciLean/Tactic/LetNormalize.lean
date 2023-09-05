2c321e066b5ae28688562b3fd59fe6b8201d0b8e
lecopivo <skrivantomas@seznam.cz>
2023-09-05 12:52:49 -0400

    tactic `let_normalize` now supports configuration

--- SciLean/Tactic/LetNormalize.lean
+++ SciLean/Tactic/LetNormalize.lean
@@ -8,18 +8,18 @@ import SciLean.Lean.Expr
 
 import SciLean.Util.RewriteBy
 
-namespace SciLean.Meta
+namespace SciLean.Meta.LetNormalize
 
 open Lean Meta Elab Tactic Conv
 
-structure NormalizeLetConfig where
+structure LetNormalizeConfig where
   removeTrivialLet := true
   removeNoFVarLet := false  
   removeLambdaLet  := true
   removeFVarProjLet := true
   splitStructureConstuctors := true
   reduceProjections := true
-
+deriving Inhabited, BEq, Repr
 
 /-- Is `e` in the form `p₁ (...(pₙ x))` where `pᵢ` are projections and `x` free variable?
 -/
@@ -102,27 +102,27 @@ let z₂ := g y
 (z₁, z₂)
 ```
 -/
-partial def normalizeLet (e : Expr) (config : NormalizeLetConfig := {}) : MetaM Expr := do
+partial def letNormalize (e : Expr) (config : LetNormalizeConfig := {}) : MetaM Expr := do
   match e.consumeMData.headBeta with
   | .letE xName xType xValue xBody _ => do
-    match (← normalizeLet xValue config).consumeMData with
+    match (← letNormalize xValue config).consumeMData with
     | .letE yName yType yValue yBody _ => 
       withLetDecl yName yType yValue fun y => 
       withLetDecl xName xType (yBody.instantiate1 y) fun x => do
-        mkLambdaFVars #[y,x] (xBody.instantiate1 x) >>= normalizeLet (config:=config)
+        mkLambdaFVars #[y,x] (xBody.instantiate1 x) >>= letNormalize (config:=config)
 
     | xValue' => do
       if (xValue'.isFVar) && config.removeTrivialLet then
-        return ← normalizeLet (xBody.instantiate1 xValue') config
+        return ← letNormalize (xBody.instantiate1 xValue') config
 
       if ¬(xValue.hasAnyFVar (fun _ => true)) && config.removeNoFVarLet then
-        return ← normalizeLet (xBody.instantiate1 xValue') config
+        return ← letNormalize (xBody.instantiate1 xValue') config
 
       if xValue'.isLambda && config.removeLambdaLet then
-        return ← normalizeLet (xBody.instantiate1 xValue') config
+        return ← letNormalize (xBody.instantiate1 xValue') config
 
       if (← isProjectionOfFVar xValue') && config.removeFVarProjLet then
-        return ← normalizeLet (xBody.instantiate1 xValue') config
+        return ← letNormalize (xBody.instantiate1 xValue') config
 
       -- deconstruct constructors into bunch of let bindings
       if config.splitStructureConstuctors then
@@ -143,22 +143,22 @@ partial def normalizeLet (e : Expr) (config : NormalizeLetConfig := {}) : MetaM 
               let xValue'' := mkAppN xValue'.getAppFn (args[0:ctor.numFields].toArray.append fvars)
               mkLambdaFVars fvars (xBody.instantiate1 xValue'')
 
-            return (← normalizeLet e' config)
+            return (← letNormalize e' config)
 
       -- in all other cases normalized the body
       withLetDecl xName xType xValue' fun x => do
-        mkLambdaFVars #[x] (← normalizeLet (xBody.instantiate1 x) config)
+        mkLambdaFVars #[x] (← letNormalize (xBody.instantiate1 x) config)
 
   | .app f x => do
-    match (← normalizeLet f) with
+    match (← letNormalize f) with
     | .letE yName yType yValue yBody _ => 
       withLetDecl yName yType yValue fun y => do
-      normalizeLet (← mkLambdaFVars #[y] (.app (yBody.instantiate1 y) x)) config
+      letNormalize (← mkLambdaFVars #[y] (.app (yBody.instantiate1 y) x)) config
     | f' => 
-      match (← normalizeLet x) with
+      match (← letNormalize x) with
       | .letE yName yType yValue yBody _ => 
         withLetDecl yName yType yValue fun y => do
-        normalizeLet (← mkLambdaFVars #[y] (.app f' (yBody.instantiate1 y))) config
+        letNormalize (← mkLambdaFVars #[y] (.app f' (yBody.instantiate1 y))) config
       | x' => do
         if config.reduceProjections then
           if let .some e' ← withReducible <| reduceProjFn?' (f'.app x') then
@@ -168,26 +168,36 @@ partial def normalizeLet (e : Expr) (config : NormalizeLetConfig := {}) : MetaM 
 
   | .lam xName xType xBody xBi => 
     withLocalDecl xName xBi xType fun x => do
-      mkLambdaFVars #[x] (← normalizeLet (xBody.instantiate1 x) config)
+      mkLambdaFVars #[x] (← letNormalize (xBody.instantiate1 x) config)
 
   | e => pure e
 
-syntax (name := let_normalize) "let_normalize" : conv 
+
+declare_config_elab elabLetNormalizeConfig  LetNormalizeConfig
+
+syntax config := atomic(" (" &"config") " := " withoutPosition(term) ")"
+
+syntax (name := let_normalize) "let_normalize" (config)? : conv 
 
 @[tactic let_normalize] 
 def convLetNormalize : Tactic
-| `(conv| let_normalize) =>   
+| `(conv| let_normalize $(cfg)?) =>   
   withTraceNode `let_normalize (fun _ => return "let_normalize") do
   (← getMainGoal).withContext do
+
+    let cfg ←
+      match cfg with
+      | .some stx => elabLetNormalizeConfig (mkNullNode #[stx])
+      | .none => pure {}
+
     let lhs ← getLhs
-    let lhs' ← normalizeLet lhs
+    let lhs' ← letNormalize lhs cfg
     
     changeLhs lhs'
 | _ => Lean.Elab.throwUnsupportedSyntax
 
 macro "let_normalize" : tactic => `(tactic| conv => let_normalize)
-
-
+macro "let_normalize" cfg:config : tactic => `(tactic| conv => let_normalize $cfg)
 
 /--
 info: let x1 := 10;
@@ -225,5 +235,5 @@ x2 + y5 + z3_fst + z3_snd : Nat
       (let z1 := 1; z1 + z1, let z2 := 2; z2 * z2)
     x3 + y5 + z3.1 + z3.2)
     rewrite_by
-      let_normalize
+      let_normalize (config := {})
 
