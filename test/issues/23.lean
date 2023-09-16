78575ce1c08a19795d089ff12e2d009f54b5d03c
lecopivo <skrivantomas@seznam.cz>
2023-09-17 01:14:29 +0200

    test for issue #23

---
+++ test/issues/23.lean
@@ -0,0 +1,22 @@
+import SciLean
+
+open SciLean
+
+variable 
+  {K : Type _} [IsROrC K]
+  {X : Type _} [Vec K X]
+
+-- example (f : X → Fin 5 → Fin 10 → Fin 15→ K) (hf : ∀ i j k, IsDifferentiable K (f · i j k))
+--   : IsDifferentiable K f := by fprop
+
+-- example (f : X → Fin 5 → Fin 10 → Fin 15→ K) (hf : ∀ i j k, IsDifferentiable K (f · i j k))
+--   : IsDifferentiable K (fun x => f x i j) := by fprop
+
+-- example (f : X → Fin 5 → Fin 10 → Fin 15→ K) (hf : ∀ i j k, IsDifferentiable K (f · i j k))
+--   : IsDifferentiable K (fun x => f x) := by fprop
+
+-- example (f : X → Fin 5 → Fin 10 → Fin 15→ K) (hf : ∀ i j k, IsDifferentiable K (f · i j k))
+--   : IsDifferentiable K (fun x i j => f x i j) := by fprop
+
+example (f : X → Fin 5 → Fin 10 → Fin 15→ K) (hf : ∀ i j k, IsDifferentiable K (f · i j k))
+  : IsDifferentiable K (fun x i j k => f x i j k) := by fprop
