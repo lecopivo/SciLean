d34e75a90e02165689a9dec63bc82c810517e50f
lecopivo <skrivantomas@seznam.cz>
2023-09-19 17:01:24 -0400

    clean up code for rewrite_by

--- SciLean/Util/RewriteBy.lean
+++ SciLean/Util/RewriteBy.lean
@@ -6,23 +6,34 @@ namespace SciLean
 open Lean.Parser.Tactic.Conv
 open Lean.Elab.Tactic.Conv
 
+open Lean Elab Term Meta 
+
+def elabConvRewrite (e : Expr) (stx : TSyntax `conv) : TermElabM (Expr × Expr) := do
+  let (rhs, eq) ← mkConvGoalFor e
+
+  let goals ← Tactic.run eq.mvarId! do
+    let (lhsNew, proof) ← convert e (Tactic.evalTactic stx)
+    updateLhs lhsNew proof
+    return ()
+
+  if goals.length = 0 then
+    throwError "this is a bug in rewriteByConv"
+
+  if goals.length > 1 then
+    throwError s!"error in `rewriteByConv`, unsolved goals {← goals.mapM (fun g => do ppExpr (← g.getType))}"
+
+  (goals.get! 0).refl
+
+  return (← instantiateMVars rhs, ← instantiateMVars eq)
+
+def rewriteByConv (e : Expr) (stx : TSyntax `conv) : MetaM (Expr × Expr) := do
+  let (r,_) ← (elabConvRewrite e stx).run {} {}
+  return r
+
 syntax:1 term "rewrite_by" convSeq : term
 
-open Lean Elab Term Meta in
 elab_rules : term
 | `($x:term rewrite_by $rw:convSeq) => do
   let x ← elabTerm x none
-  let (_, eq) ← mkConvGoalFor x
-
-  let goals ← Tactic.run eq.mvarId! do
-    let (lhsNew, proof) ← convert x (Tactic.evalTactic (← `(conv| ($rw))))
-    updateLhs lhsNew proof
-    return ()
-  
-  if goals.length = 0 then
-    throwError "this is a bug in rewrite_by"
-
-  if goals.length > 1 then
-    throwError s!"unsolved goals {← goals.mapM (fun g => do ppExpr (← g.getType))}"
-  
-  return (← (goals.get! 0).getType).consumeMData.getArg! 1
+  let (x',_eq) ← elabConvRewrite x (← `(conv| ($rw)))
+  return x'
