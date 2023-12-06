996ae91e7471ee398fcf8bf03f5ce45038692b40
lecopivo <skrivantomas@seznam.cz>
2023-12-06 06:02:40 +0100

    add ML.lean file

---
+++ SciLean/Modules/ML.lean
@@ -0,0 +1,8 @@
+import SciLean.Modules.ML.Activation
+import SciLean.Modules.ML.Convolution
+import SciLean.Modules.ML.Dense
+import SciLean.Modules.ML.Loss
+import SciLean.Modules.ML.SoftMax
+import SciLean.Modules.ML.Pool
+
+import SciLean.Modules.ML.MNIST
