ec6b3ba8f939482027383bcb0b4e73b99311063e
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2023-12-17 23:25:34 +0100

    Update harmonic_oscillator.lean

--- doc/literate/harmonic_oscillator.lean
+++ doc/literate/harmonic_oscillator.lean
@@ -1,6 +1,6 @@
 import SciLean.Mechanics
 import SciLean.Operators.ODE
--- import SciLean.Solver 
+-- import SciLean.Solver
 -- import SciLean.Tactic.LiftLimit
 -- import SciLean.Tactic.FinishImpl
 import SciLean.Solver.Solver
@@ -8,14 +8,14 @@ import SciLean.Solver.Solver
 open SciLean
 
 
-/-! 
+/-!
 
 ==============================
 Harmonic Oscillator in SciLean
 ==============================
 
-Let's demonstrate basic capabilities of SciLean on the simulation of 
-harmonic oscillator. 
+Let's demonstrate basic capabilities of SciLean on the simulation of
+harmonic oscillator.
 
 The energy of harmonic oscillator is
 
@@ -32,14 +32,14 @@ def H (m k : ℝ) (x p : ℝ) := 1/(2*m) * ∥p∥² + k/2 * ∥x∥²
 
 To compute a derivative of a function `f : ℝ → ℝ`
 
-.. math:: \frac{\partial}{\partial x} f(x) \qquad \text{or} \qquad \frac{\partial}{\partial x'}\bigg\rvert_{x'=x} f(x') 
+.. math:: \frac{\partial}{\partial x} f(x) \qquad \text{or} \qquad \frac{\partial}{\partial x'}\bigg\rvert_{x'=x} f(x')
 
 we write `∇ x, f x` or `λ x => ∇ (x':=x), f x'`.
 
 The derivative w.r.t position of the Hamiltonian is
 -/
 
-#check (λ m k x p => ∇ (x':=x), H m k x' p) 
+#check (λ m k x p => ∇ (x':=x), H m k x' p)
   rewrite_by (simp[H,gradient]; trace_state) /- .unfold -/
 
 
@@ -48,22 +48,22 @@ The derivative w.r.t position of the Hamiltonian is
 And the derivative w.r.t momentum of the Hamiltonian is
  -/
 
-#check (λ m k x p => ∇ (p':=p), H m k x p') 
+#check (λ m k x p => ∇ (p':=p), H m k x p')
   rewrite_by (simp[H,gradient,hold]; trace_state) /- .unfold -/
 
 
 /-!
 
 To run a simulation means to numerically solve some differential equation.
-Surprisingly, SciLean defines a function `ode_solve` that solves a generic 
+Surprisingly, SciLean defines a function `ode_solve` that solves a generic
 differential equation exactly
 
 .. math:: \dot x = f(x) \qquad x(0) = x_0
- 
-The catch is that this function is not computable. This means there is 
-no universal algorithm that would give you a solution of the above 
-differential equation for any `f`. Nontheless, `ode_solve` is well 
-defined and in SciLean we use it to specify what kind of program we 
+
+The catch is that this function is not computable. This means there is
+no universal algorithm that would give you a solution of the above
+differential equation for any `f`. Nonetheless, `ode_solve` is well
+defined and in SciLean we use it to specify what kind of program we
 want to build.
 
 Let's have a look at the type signature of `ode_solve`
@@ -80,10 +80,10 @@ For harmonic oscillator, the evolution is given by Hamilton's equations
 
   \begin{align}
   \dot x &= \frac{\partial H}{\partial p} \\
-  \dot p &= - \frac{\partial H}{\partial x} 
+  \dot p &= - \frac{\partial H}{\partial x}
   \end{align}
 
-Thus the function `f` that needs to be provided to `ode_solve` is 
+Thus the function `f` that needs to be provided to `ode_solve` is
 `λ (x,p) => (∇ (p':=p), H x p', - ∇ (x':=x), H x' p)`
 
 -/
@@ -93,20 +93,20 @@ Thus the function `f` that needs to be provided to `ode_solve` is
 
 /-!
 
-Now we turn in to a very interesing style of programming. We provide 
+Now we turn in to a very interesting style of programming. We provide
 a specification and with a series of commands(tactics) we construct a runnable
 program.
 
 To run a simulation on a computer we want an implementation of a differential
-equation solver i.e. we want a withness of `Impl (ode_solve f)`.
+equation solver i.e. we want a witness of `Impl (ode_solve f)`.
 
 For harmonic oscillator this is
 
  -/
 
 -- set_option trace.Meta.Tactic.simp.rewrite true in
-approx solver (steps : Nat) := λ (m k : ℝ) => 
-  (ode_solve λ (x,p) => (  ∇ (p':=p), H m k x p', 
+approx solver (steps : Nat) := λ (m k : ℝ) =>
+  (ode_solve λ (x,p) => (  ∇ (p':=p), H m k x p',
                          - ∇ (x':=x), H m k x' p))
 by
   -- unfold Hamiltonian definition
@@ -118,7 +118,7 @@ by
   -- apply RK4 method
   rw [ode_solve_fixed_dt runge_kutta4_step]
 
-  -- agree that we solve the original goal only approximately 
+  -- agree that we solve the original goal only approximately
   bubble_limit; sorry_proof; apply Approx.limit _ steps; intro n; simp
 
 
@@ -141,7 +141,7 @@ def main : IO Unit := do
   let mut (x,p) := (x₀, p₀)
 
   for _ in [0:40] do
-  
+
     (x, p) := evolve 0.1 (x, p)
 
     -- print
