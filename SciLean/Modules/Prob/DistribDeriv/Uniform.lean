54a6ec90e8fee934ed0d2428b7fe9f53e88963da
lecopivo <skrivantomas@seznam.cz>
2024-02-12 01:13:42 +0100

    uniform distribution

---
+++ SciLean/Modules/Prob/DistribDeriv/Uniform.lean
@@ -0,0 +1,129 @@
+import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
+import Mathlib.Analysis.Calculus.FDeriv.Mul
+
+import SciLean.Modules.Prob.DistribDeriv.DistribDeriv
+import SciLean.Modules.Prob.DistribDeriv.DistribFwdDeriv
+import SciLean.Modules.Prob.DerivUnderIntegralSign
+
+namespace SciLean.Prob
+
+variable
+  {W} [NormedAddCommGroup W] [NormedSpace ℝ W] [FiniteDimensional ℝ W] [MeasurableSpace W]
+  {X} [NormedAddCommGroup X] [NormedSpace ℝ X] [FiniteDimensional ℝ X] [MeasurableSpace X]
+  {Y} [NormedAddCommGroup Y] [NormedSpace ℝ Y] [FiniteDimensional ℝ Y] [MeasurableSpace Y]
+  {Z} [NormedAddCommGroup Z] [NormedSpace ℝ Z] [FiniteDimensional ℝ Z] [MeasurableSpace Z]
+
+
+open MeasureTheory
+
+
+noncomputable
+def uniform (a b : ℝ) : Distribution ℝ := fun φ => (b-a)⁻¹ * ∫ x in Set.uIcc a b, φ x
+
+noncomputable
+def duniform (a b da db : ℝ) : Distribution ℝ :=
+  fun φ => (b - a)⁻¹ * (db * φ b - da * φ a - (db - da) * (b - a)⁻¹ * ∫ x in Set.uIcc a b, φ x)
+
+noncomputable
+def fduniform (a b da db : ℝ) : FDistribution ℝ := {
+  val := uniform a b
+  dval := duniform a b da db
+}
+
+-- I think we migh be able to remove the condition `a x ≠ b x`
+-- @[fprop]
+theorem uniform.differentiableAt (a b : X → ℝ) (φ : ℝ → ℝ) (x : X) (hab : a x ≠ b x)
+    (ha : DifferentiableAt ℝ a x) (hb : DifferentiableAt ℝ b x)
+    (hφa : ContinuousAt φ (a x)) (hφb : ContinuousAt φ (b x)) : -- also integrability condition
+    DifferentiableAt ℝ (fun x => uniform (a x) (b x) φ) x := by dsimp[uniform]; sorry
+
+
+
+-- @[fprop]
+theorem uniform.bind._arg_xf.differentiableAt (a b : X → ℝ) (f : X → ℝ → Distribution Z) (φ : Z → ℝ) (x : X) (hab : a x ≠ b x)
+    (ha : DifferentiableAt ℝ a x) (hb : DifferentiableAt ℝ b x)
+    -- TODO: weaken 'hf' such that we still need `ha` and `hb`
+    (hf : DifferentiableUnderIntegralAt (fun x y => f x y φ) (fun x' => volume.restrict (Set.uIcc (a x') (b x'))) x) :
+    DifferentiableAt ℝ (fun x => (uniform (a x) (b x)).bind (f x) φ) x := by
+
+  simp[uniform,Distribution.bind]
+  apply DifferentiableAt.mul
+  . sorry
+  . apply hf.diff
+
+
+
+
+@[simp]
+theorem uniform.distribDeriv_comp
+    (a b : X → ℝ) (x dx : X) (φ : ℝ → ℝ) (hab : a x ≠ b x)
+    (ha : DifferentiableAt ℝ a x) (hb : DifferentiableAt ℝ b x)
+    (hφa : ContinuousAt φ (a x)) (hφb : ContinuousAt φ (b x))
+    /- integrability condition on φ -/:
+    distribDeriv (fun x : X => uniform (a x) (b x)) x dx φ
+    =
+    let a' := a x
+    let da := fderiv ℝ a x dx
+    let b' := b x
+    let db := fderiv ℝ b x dx
+    duniform a' b' da db φ := by
+
+  simp[uniform,duniform,Distribution.bind,distribDeriv]
+  rw[fderiv_mul (by sorry) (by sorry)]
+  sorry
+
+
+@[simp]
+theorem uniform.bind.arg_xf.distribDeriv_rule
+    (a b : X → ℝ) (f : X → ℝ → Distribution Z) (x dx) (φ : Z → ℝ) (hab : a x ≠ b x)
+    (ha : DifferentiableAt ℝ a x) (hb : DifferentiableAt ℝ b x)
+    -- TODO: weaken 'hf' such that we still need `ha` and `hb`
+    (hf : DifferentiableUnderIntegralAt (fun x y => f x y φ) (fun x' => volume.restrict (Set.uIcc (a x') (b x'))) x) :
+    distribDeriv (fun x' => (uniform (a x') (b x')).bind (f x')) x dx φ
+    =
+    let a' := a x
+    let da := fderiv ℝ a x dx
+    let b' := b x
+    let db := fderiv ℝ b x dx
+    (duniform a' b' da db).bind (f x ·) φ
+    +
+    (uniform a' b').bind (fun y => distribDeriv (f · y) x dx) φ := by
+
+  simp [distribDeriv, uniform, duniform, Distribution.bind]
+  rw[fderiv_mul (by sorry) (by sorry)]
+
+  sorry
+
+
+@[simp]
+theorem uniform.distribFwdDeriv_comp
+    (a b : X → ℝ) (x dx : X) (φ : ℝ → ℝ) (hab : a x ≠ b x)
+    (ha : DifferentiableAt ℝ a x) (hb : DifferentiableAt ℝ b x)
+    (hφa : ContinuousAt φ (a x)) (hφb : ContinuousAt φ (b x))
+    /- integrability condition on φ -/:
+    distribFwdDeriv (fun x : X => uniform (a x) (b x)) x dx φ
+    =
+    let ada := fwdFDeriv ℝ a x dx
+    let bdb := fwdFDeriv ℝ b x dx
+    fduniform ada.1 bdb.1 ada.2 bdb.2 φ := by
+
+  unfold distribFwdDeriv
+  simp (disch := assumption) only [FDistribution_apply, distribDeriv_comp]
+  rfl
+
+
+
+@[simp]
+theorem uniform.bind.arg_xf.distribFwdDeriv_rule
+    (a b : X → ℝ) (f : X → ℝ → Distribution Z) (x dx) (φ : Z → ℝ) (hab : a x ≠ b x)
+    (ha : DifferentiableAt ℝ a x) (hb : DifferentiableAt ℝ b x)
+    -- TODO: weaken 'hf' such that we still need `ha` and `hb`
+    (hf : DifferentiableUnderIntegralAt (fun x y => f x y φ) (fun x' => volume.restrict (Set.uIcc (a x') (b x'))) x) :
+    distribFwdDeriv (fun x' => (uniform (a x') (b x')).bind (f x')) x dx φ
+    =
+    let ada := fwdFDeriv ℝ a x dx
+    let bdb := fwdFDeriv ℝ b x dx
+    (fduniform ada.1 bdb.1 ada.2 bdb.2).bind (fun y => distribFwdDeriv (f · y) x dx) φ := by
+
+  unfold distribFwdDeriv fduniform fwdFDeriv
+  simp (disch := assumption) only [FDistribution_apply, distribDeriv_rule, FDistribution.bind, Pi.add_apply]
