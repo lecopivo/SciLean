81936a9462a06b3c4808b8bd2587b336abcfc125
lecopivo <skrivantomas@seznam.cz>
2024-02-21 15:56:38 -0500

    clean up semiAdjoint

--- SciLean/Core/FunctionTransformations/SemiAdjoint.lean
+++ SciLean/Core/FunctionTransformations/SemiAdjoint.lean
@@ -1,11 +1,8 @@
 import SciLean.Core.FunctionPropositions.HasSemiAdjoint
 
-import SciLean.Tactic.FTrans.Basic
-
 set_option linter.unusedVariables false
 
 open LeanColls
-
 namespace SciLean
 
 variable
@@ -21,141 +18,47 @@ namespace semiAdjoint
 -- Basic lambda calculus rules -------------------------------------------------
 --------------------------------------------------------------------------------
 
-variable (X)
-theorem id_rule
-  : semiAdjoint K (fun (x : X) => x) = fun x => x := by sorry_proof
+@[fun_trans]
+theorem id_rule : semiAdjoint K (fun (x : X) => x) = fun x => x := by sorry_proof
 
-variable (Y)
-theorem const_rule
-  : semiAdjoint K (fun (_ : X) => (0 : Y)) = fun x => 0 :=
-by sorry_proof
-variable {X Y}
+-- todo: make this work with fun_trans properly
+@[fun_trans]
+theorem const_rule : semiAdjoint K (fun (_ : X) => (0 : Y)) = fun x => 0 := by sorry_proof
 
-variable (E)
-theorem proj_rule [DecidableEq ι]
-   (i : ι)
-  : semiAdjoint K (fun (f : (i:ι) → E i) => f i)
-    =
-    fun y => (fun j => if h : i=j then h▸y else 0)
-  := sorry_proof
-variable {E}
-
+@[fun_trans]
 theorem comp_rule
-  (f : Y → Z) (g : X → Y)
-  (hf : HasSemiAdjoint K f) (hg : HasSemiAdjoint K g)
-  : semiAdjoint K (fun x => f (g x))
+  (f : Y → Z) (g : X → Y) (hf : HasSemiAdjoint K f) (hg : HasSemiAdjoint K g) :
+    semiAdjoint K (fun x => f (g x))
     =
     fun z =>
       let y := semiAdjoint K f z
       let x := semiAdjoint K g y
-      x :=
-by sorry_proof
+      x := by sorry_proof
 
+@[fun_trans]
 theorem let_rule
-  (f : X → Y → Z) (g : X → Y)
-  (hf : HasSemiAdjoint K (fun xy : X×Y => f xy.1 xy.2)) (hg : HasSemiAdjoint K g)
-  : semiAdjoint K (fun x => let y := g x; f x y)
+    (f : X → Y → Z) (g : X → Y)
+    (hf : HasSemiAdjoint K ↿f) (hg : HasSemiAdjoint K g) :
+    semiAdjoint K (fun x => let y := g x; f x y)
     =
     fun z =>
       let xy := semiAdjoint K (fun xy : X×Y => f xy.1 xy.2) z
       let x' := semiAdjoint K g xy.2
-      xy.1 + x' :=
-by sorry_proof
+      xy.1 + x' := by sorry_proof
 
-theorem pi_rule
-  (f : X → (i : ι) → E i) (hf : ∀ i, HasSemiAdjoint K (f · i))
-  : semiAdjoint K (fun (x : X) (i : ι) => f x i)
+@[fun_trans]
+theorem apply_rule [DecidableEq ι] (i : ι) :
+    semiAdjoint K (fun (f : (i:ι) → E i) => f i)
     =
-    (fun x' => ∑ i, semiAdjoint K (fun x => f x i) (x' i))
-  := sorry_proof
+    fun y => (fun j => if h : i=j then h▸y else 0) := sorry_proof
 
+@[fun_trans]
+theorem pi_rule
+    (f : X → (i : ι) → E i) (hf : ∀ i, HasSemiAdjoint K (f · i)) :
+    semiAdjoint K (fun (x : X) (i : ι) => f x i)
+    =
+    (fun x' => ∑ i, semiAdjoint K (fun x => f x i) (x' i)) := sorry_proof
 
-
--- Register `adjoint` as function transformation -------------------------------
---------------------------------------------------------------------------------
-
-open Lean Meta Qq
-
-def discharger (e : Expr) : SimpM (Option Expr) := do
-  withTraceNode `fwdDeriv_discharger (fun _ => return s!"discharge {← ppExpr e}") do
-  let cache := (← get).cache
-  let config : FProp.Config := {}
-  let state  : FProp.State := { cache := cache }
-  let (proof?, state) ← FProp.fprop e |>.run config |>.run state
-  modify (fun simpState => { simpState with cache := state.cache })
-  if proof?.isSome then
-    return proof?
-  else
-    -- if `fprop` fails try assumption
-    let tac := FTrans.tacticToDischarge (Syntax.mkLit ``Lean.Parser.Tactic.assumption "assumption")
-    let proof? ← tac e
-    return proof?
-
-open Lean Elab Term FTrans
-def ftransExt : FTransExt where
-  ftransName := ``semiAdjoint
-
-  getFTransFun? e :=
-    if e.isAppOf ``semiAdjoint then
-
-      if let .some f := e.getArg? 6 then
-        some f
-      else
-        none
-    else
-      none
-
-  replaceFTransFun e f :=
-    if e.isAppOf ``semiAdjoint then
-      e.setArg 6 f
-    else
-      e
-
-  idRule  e X := do
-    let K := e.getArg! 0
-    tryTheorems
-      #[ { proof := ← mkAppM ``id_rule #[K, X], origin := .decl ``id_rule, rfl := false} ]
-      discharger e
-
-  constRule e X y := do
-    let K := e.getArg! 0
-    tryTheorems
-      #[ { proof := ← mkAppM ``const_rule #[K, X, (← inferType y)], origin := .decl ``const_rule, rfl := false} ]
-      discharger e
-
-  projRule e X i := do
-    let K := e.getArg! 0
-    tryTheorems
-      #[ { proof := ← mkAppM ``proj_rule #[K, X, i], origin := .decl ``proj_rule, rfl := false} ]
-      discharger e
-
-  compRule e f g := do
-    let K := e.getArg! 0
-    tryTheorems
-      #[ { proof := ← withTransparency .all <|
-             mkAppM ``comp_rule #[K, f, g], origin := .decl ``comp_rule, rfl := false} ]
-      discharger e
-
-  letRule e f g := do
-    let K := e.getArg! 0
-    tryTheorems
-      #[ { proof := ← mkAppM ``let_rule #[K, f, g], origin := .decl ``let_rule, rfl := false} ]
-      discharger e
-
-  piRule  e f := do
-    let K := e.getArg! 0
-    tryTheorems
-      #[ { proof := ← mkAppM ``pi_rule #[K, f], origin := .decl ``pi_rule, rfl := false} ]
-      discharger e
-
-  discharger := discharger
-
-
--- register adjoint
-#eval show Lean.CoreM Unit from do
-  modifyEnv (λ env => FTrans.ftransExt.addEntry env (``semiAdjoint, ftransExt))
-
-end SciLean.semiAdjoint
 
 --------------------------------------------------------------------------------
 -- Function Rules --------------------------------------------------------------
@@ -178,7 +81,7 @@ open SciLean
 -- Prod.mk ---------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[ftrans]
+@[fun_trans]
 theorem Prod.mk.arg_fstsnd.semiAdjoint_rule
   (g : X → Y) (f : X → Z)
   (hg : HasSemiAdjoint K g) (hf : HasSemiAdjoint K f)
@@ -194,7 +97,7 @@ by sorry_proof
 -- Prod.fst --------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[ftrans]
+@[fun_trans]
 theorem Prod.fst.arg_self.semiAdjoint_rule
   (f : X → Y×Z) (hf : SciLean.HasSemiAdjoint K f)
   : semiAdjoint K (fun x => (f x).1)
@@ -207,7 +110,7 @@ by
 -- Prod.snd --------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[ftrans]
+@[fun_trans]
 theorem Prod.snd.arg_self.semiAdjoint_rule
   (f : X → Y×Z) (hf : SciLean.HasSemiAdjoint K f)
   : semiAdjoint K (fun x => (f x).2)
@@ -220,7 +123,7 @@ by
 -- HAdd.hAdd -------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[ftrans]
+@[fun_trans]
 theorem HAdd.hAdd.arg_a0a1.semiAdjoint_rule
   (f g : X → Y) (hf : HasSemiAdjoint K f) (hg : HasSemiAdjoint K g)
   : semiAdjoint K (fun x => f x + g x)
@@ -236,7 +139,7 @@ by
 -- HSub.hSub -------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[ftrans]
+@[fun_trans]
 theorem HSub.hSub.arg_a0a1.semiAdjoint_rule
   (f g : X → Y) (hf : HasSemiAdjoint K f) (hg : HasSemiAdjoint K g)
   : semiAdjoint K (fun x => f x - g x)
@@ -251,7 +154,7 @@ by sorry_proof
 -- Neg.neg ---------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[ftrans]
+@[fun_trans]
 theorem Neg.neg.arg_a0.semiAdjoint_rule
   (f : X → Y)
   : semiAdjoint K (fun x => - f x)
@@ -265,7 +168,7 @@ by
 --------------------------------------------------------------------------------
 
 open ComplexConjugate in
-@[ftrans]
+@[fun_trans]
 theorem HMul.hMul.arg_a0.semiAdjoint_rule
   (c : K) (f : X → K) (hf : HasSemiAdjoint K f)
   : semiAdjoint K (fun x => f x * c)
@@ -275,7 +178,7 @@ by
   sorry_proof
 
 open ComplexConjugate in
-@[ftrans]
+@[fun_trans]
 theorem HMul.hMul.arg_a1.semiAdjoint_rule
   (c : K) (f : X → K) (hf : HasSemiAdjoint K f)
   : semiAdjoint K (fun x => c * f x)
@@ -288,7 +191,7 @@ by sorry_proof
 --------------------------------------------------------------------------------
 
 open ComplexConjugate in
-@[ftrans]
+@[fun_trans]
 theorem HSMul.hSMul.arg_a0.semiAdjoint_rule
   {Y : Type _} [SemiHilbert K Y]
   (y' : Y) (f : X → K) (hf : HasSemiAdjoint K f)
@@ -299,7 +202,7 @@ by
   sorry_proof
 
 open ComplexConjugate in
-@[ftrans]
+@[fun_trans]
 theorem HSMul.hSMul.arg_a1.semiAdjoint_rule
   (c : K) (g : X → Y) (hg : HasSemiAdjoint K g)
   : semiAdjoint K (fun x => c • g x)
@@ -312,7 +215,7 @@ by sorry_proof
 --------------------------------------------------------------------------------
 
 open ComplexConjugate in
-@[ftrans]
+@[fun_trans]
 theorem HDiv.hDiv.arg_a0.semiAdjoint_rule
   (f : X → K) (c : K)
   (hf : HasSemiAdjoint K f)
@@ -327,7 +230,7 @@ by
 --------------------------------------------------------------------------------
 
 open BigOperators in
-@[ftrans]
+@[fun_trans]
 theorem Finset.sum.arg_f.semiAdjoint_rule {ι : Type _} [Fintype ι]
   (f : X → ι → Y) (hf : ∀ i, HasSemiAdjoint K (f · i))
   : semiAdjoint K (fun x => ∑ i, f x i)
@@ -337,11 +240,11 @@ by
   sorry_proof
 
 
--- EnumType.sum ------------------------------------------------------------------
+-- IndexType.sum ------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[ftrans]
-theorem SciLean.EnumType.sum.arg_f.semiAdjoint_rule
+@[fun_trans]
+theorem IndexType.sum.arg_f.semiAdjoint_rule
   (f : X → ι → Y) (hf : ∀ i, HasSemiAdjoint K (f · i))
   : semiAdjoint K (fun x => ∑ i, f x i)
     =
@@ -353,7 +256,7 @@ by
 -- d/ite -----------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[ftrans]
+@[fun_trans]
 theorem ite.arg_te.semiAdjoint_rule
   (c : Prop) [dec : Decidable c] (t e : X → Y)
   : semiAdjoint K (fun x => ite c (t x) (e x))
@@ -365,7 +268,7 @@ by
   case isTrue h  => ext y; simp[h]
   case isFalse h => ext y; simp[h]
 
-@[ftrans]
+@[fun_trans]
 theorem dite.arg_te.semiAdjoint_rule
   (c : Prop) [dec : Decidable c]
   (t : c  → X → Y) (e : ¬c → X → Y)
@@ -384,7 +287,7 @@ by
 --------------------------------------------------------------------------------
 
 open ComplexConjugate in
-@[ftrans]
+@[fun_trans]
 theorem Inner.inner.arg_a0.semiAdjoint_rule
   {Y : Type _} [SemiHilbert K Y]
   (f : X → Y) (hf : HasSemiAdjoint K f) (y : Y)
@@ -393,7 +296,7 @@ theorem Inner.inner.arg_a0.semiAdjoint_rule
     fun z => (conj z) • semiAdjoint K f y :=
 by sorry_proof
 
-@[ftrans]
+@[fun_trans]
 theorem Inner.inner.arg_a1.semiAdjoint_rule
   {Y : Type _} [SemiHilbert K Y]
   (f : X → Y) (hf : HasSemiAdjoint K f) (y : Y)
@@ -405,9 +308,9 @@ by sorry_proof
 
 -- conj/starRingEnd ------------------------------------------------------------
 --------------------------------------------------------------------------------
-set_option linter.ftransDeclName false in
+
 open ComplexConjugate in
-@[ftrans]
+@[fun_trans]
 theorem starRingEnd.arg_a0.semiAdjoint_rule
   (f : X → K)
   : semiAdjoint K (fun x => conj (f x))
@@ -419,7 +322,7 @@ by sorry_proof
 -- semiAdjoint -----------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[ftrans]
+@[fun_trans]
 theorem SciLean.semiAdjoint.arg_y.semiAdjoint_rule
   (f : X → Y) (a3 : W → Y) (hf : HasSemiAdjoint K f) (ha3 : HasSemiAdjoint K a3)
   : semiAdjoint K (fun w => semiAdjoint K f (a3 w))
