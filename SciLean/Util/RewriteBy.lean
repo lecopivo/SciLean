2eb00fb18cf3679eaf17f043ca133b00995ebc42
lecopivo <skrivantomas@seznam.cz>
2024-02-25 21:20:55 +0100

    allow adding assumptions to rewrite_by notation

--- SciLean/Util/RewriteBy.lean
+++ SciLean/Util/RewriteBy.lean
@@ -8,11 +8,15 @@ open Lean.Elab.Tactic.Conv
 
 open Lean Elab Term Meta
 
-def elabConvRewrite (e : Expr) (stx : TSyntax `conv) : TermElabM (Expr × Expr) := do
+def elabConvRewrite (e : Expr) (assumptions : Array Syntax) (stx : TSyntax `conv) : TermElabM (Expr × Expr) := do
+  elabBinders assumptions fun as => do
   let (rhs, eq) ← mkConvGoalFor e
 
   let goals ← Tactic.run eq.mvarId! do
+
     let (lhsNew, proof) ← convert e (Tactic.evalTactic stx)
+    if let .some a := as.find? (fun a => lhsNew.containsFVar a.fvarId!) then
+      throwError "rewritten expreesion can't contain assumption {← ppExpr a}"
     updateLhs lhsNew proof
     return ()
 
@@ -27,14 +31,16 @@ def elabConvRewrite (e : Expr) (stx : TSyntax `conv) : TermElabM (Expr × Expr) 
   return (← instantiateMVars rhs, ← instantiateMVars eq)
 
 def rewriteByConv (e : Expr) (stx : TSyntax `conv) : MetaM (Expr × Expr) := do
-  let (r,_) ← (elabConvRewrite e stx).run {} {}
+  let (r,_) ← (elabConvRewrite e #[] stx).run {} {}
   return r
 
-syntax:1 term "rewrite_by" convSeq : term
+
+syntax:1 term "rewrite_by" (" assuming" bracketedBinder*)?  convSeq : term
 
 elab_rules : term
-| `($x:term rewrite_by $rw:convSeq) => do
+| `($x:term rewrite_by $[assuming $as*]? $rw:convSeq) => do
   let x ← elabTerm x none
+  let as := as.getD #[]
   synthesizeSyntheticMVarsNoPostponing
-  let (x',_eq) ← elabConvRewrite x (← `(conv| ($rw)))
+  let (x',_eq) ← elabConvRewrite x as (← `(conv| ($rw)))
   return x'
