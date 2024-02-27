15d7275dacfd79a91cd8891a432b3cccd33deea0
lecopivo <skrivantomas@seznam.cz>
2024-02-27 13:21:15 -0500

    progress on walkonspeheres

--- SciLean/Core/Rand/Distributions/WalkOnSpheres.lean
+++ SciLean/Core/Rand/Distributions/WalkOnSpheres.lean
@@ -3,6 +3,9 @@ import SciLean.Core.Rand.Tactic
 import SciLean.Core.FunctionTransformations
 import SciLean.Core.Notation.CDeriv
 import SciLean.Core.Notation.FwdDeriv
+import SciLean.Tactic.LSimp.LetNormalize
+
+import SciLean.Data.DataArray
 
 -- import Mathlib.MeasureTheory.Measure.Haar.Basic
 -- import Mathlib.MeasureTheory.Constructions.Pi
@@ -17,7 +20,7 @@ variable
   {R} [RealScalar R]
   {ι} [IndexType ι] [LawfulIndexType ι] [DecidableEq ι]
   {X} [FinVec ι R X] [Module ℝ X] [MeasureSpace X]
-  {Y} [SemiHilbert R Y] [Module ℝ Y]
+  {Y} [SemiHilbert R Y] [Module ℝ Y] [IsScalarTower ℝ R Y]
   {U} [Vec R U] [Module ℝ U]
   {W} [Vec R W] [Module ℝ W]
   {α} [MeasurableSpace α]
@@ -27,66 +30,30 @@ set_default_scalar R
 def sphere (x : X) (r : R) := {y : X // ‖y-x‖₂[R] = r}
 def ball   (x : X) (r : R) := {y : X  | ‖y-x‖₂[R] < r}
 
+abbrev π : R := RealScalar.pi
+
+variable [PlainDataType R]
+#check DataArrayN R (Fin 3)
+
+#check R^[Fin 3]
+
 instance (x : X) (r : R) : MeasureSpace (sphere x r) := sorry
 
 #check ∫' (y : (sphere (0:X) (1:R))), y.1
 
 def harmonic_function (u : X → Y) : Prop := ∀ x (r : R), u x = ∫' (y : sphere x r), u y.1
 
-def poisson_function (f : X → Y) (u : X → Y) : Prop := ∀ x (r : R), u x = ∫' (y : sphere x r), u y.1 + ∫' y in ball x r, f y
-
 theorem integral_to_unit_sphere (x : X) (r : R) (u : X → Y) :
     (∫' (x' : sphere x r), u x'.1)
     =
     r^2 • ∫' (x' : sphere (0:X) (1:R)), u (x + r • x'.1) := sorry_proof
 
-#exit
-variable
-    {C : ℕ → Type} [∀ n, Vec R (C n)] [∀ n, MeasurableSpace (C n)]
-    {D : ℕ → Type} [∀ n, MeasurableSpace (D n)]
-
-
--- This is true only for probability measures!!!!
--- for general measures it is true only for linear maps
-open Measure in
-@[rand_pull_E]
-theorem pull_integral_nat_recOn (x₀ : C 0) (μ : (n : Nat) → Measure (D n))
-    (f : (n : ℕ) → C n → D n → (C (n+1))) (hf : ∀ n d, IsAffineMap R (f n · d)) :
-    Nat.recOn (motive := C)
-      n
-      x₀
-      (fun n x => ∫' y', (f n x y') ∂(μ n))
-    =
-    ∫' y', y'
-      ∂(Nat.recOn (motive:=fun n => Measure (C n)) n
-         (dirac x₀)
-         (fun n x =>
-           Measure.bind x fun x' =>
-           Measure.bind (μ n) fun y' =>
-           dirac (f n x' y'))) := sorry
-  -- induction n
-  -- case zero => simp[mean]
-  -- case succ n hn =>
-  --   simp[hn,mean]
-  --   conv => simp[rand_pull_E]
-  --   conv =>
-  --     lhs
-  --     enter[1,2,x',1]
-  --     unfold mean
-  --     simp[pull_E_affine (f:=(f n · x')) (hf:=hf n x')]
-  --   conv =>
-  --     simp[rand_pull_E]
-  --   rw[Rand.swap_bind]
-
-
-
---variable (φ : X → R) (φ' : X → X) (hφ : gradient φ = φ') (g : X → Y)
 
 noncomputable
 def harmonicRec (n : ℕ) (φ : X → R) (g : X → Y) (x : X) : Y :=
   match n with
   | 0 => g x
-  | m+1 => ∫' (x' : sphere (0:X) (1:R)), harmonicRec m φ g (x + φ x • x'.1)
+  | m+1 => (4*π:R)⁻¹ • ∫' (x' : sphere (0:X) (1:R)), harmonicRec m φ g (x + φ x • x'.1)
 
 
 @[fun_prop]
@@ -97,7 +64,8 @@ theorem harmonicRec.arg_x.CDifferentiable_rule (n : ℕ)
   induction n <;> (simp[harmonicRec]; fun_prop)
 
 
--- set_option trace.Meta.Tactic.fun_trans true in
+set_option trace.Meta.Tactic.fun_trans true in
+
 noncomputable
 def harmonicRec.arg_x.cderiv (n : ℕ)
     (φ : X → R) (φ' : X → X → R) (g : X → Y) (g' : X → X → Y) :=
@@ -105,10 +73,9 @@ def harmonicRec.arg_x.cderiv (n : ℕ)
   rewrite_by
     assuming (hφ' : ∂ φ = φ') (hφ : CDifferentiable R φ)
              (hg' : ∂ g = g') (hg : CDifferentiable R g)
-    induction n n' prev h
+    induction n n' du h
       . simp[harmonicRec]; fun_trans
-      . simp[harmonicRec]
-        fun_trans
+      . simp only [harmonicRec]; fun_trans only [ftrans_simp]
 
 -- set_option trace.Meta.Tactic.fun_trans true in
 noncomputable
@@ -120,34 +87,46 @@ def harmonicRec.arg_x.fwdDeriv (n : ℕ)
              (hg' : ∂> g = g') (hg : CDifferentiable R g)
     induction n n' prev h
       . simp[harmonicRec]; fun_trans
-      . simp[harmonicRec]
-        rw[integral_fwdDeriv (hf:=by fun_prop)]
-        enter [w,dw,1,x]
+      . simp[harmonicRec]; fun_trans
 
 variable
   (n : ℕ)
   (φ : X → R) (g : X → Y)
   (hφ : CDifferentiable R φ) (hg : CDifferentiable R g)
 
-
 variable [MeasurableSpace Y]
 
+
+class UniformRand (X : Type _) where
+  uniform : Rand X
+
+instance [FinVec ι R X] (x : X) (r : R) : UniformRand (sphere x r) where
+  uniform := sorry
+
+export UniformRand (uniform)
+
+#check UniformRand.uniform.ℙ
+#check volume
+
+-- todo: scale by density!!!
+theorem integral_as_uniform_E {Y} [AddCommGroup Y] [Module ℝ Y] -- todo: change to locally convex space
+    (f : X → Y) [UniformRand X] :
+    ∫' (x : X), f x = uniform.E f := sorry
 
 set_option pp.funBinderTypes true in
 def walkOnSpheres (φ : X → R) (g : X → Y) (n : ℕ) (x : X) : Rand Y := do
   let f : Rand (X → Y) :=
     derive_random_approx
-      (fun x => harmonicRec φ g n x)
+      (fun x => harmonicRec n φ g x)
     by
       induction n n' prev h
         . simp[harmonicRec]
         . simp[harmonicRec,h]
-          simp[integral_lambda_push]
-      rw[pull_integral_nat_recOn (R:=R) (x₀:=_) (μ:=_) (hf:=by fun_prop)]
+          simp only [smul_push,cintegral.arg_f.push_lambda]
+          rw[integral_as_uniform_E]
+      rw[pull_E_nat_recOn (x₀:=_) (r:=_) (hf:=by fun_prop)]
   let f' ← f
   pure (f' x)
-
-
 
 variable (u : X → Y) (φ : X → R) (hu : CDifferentiable R u) (hφ : CDifferentiable R φ)
 
