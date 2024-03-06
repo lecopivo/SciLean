75dda7d23f010ee2ba13f34d552e1f58f7a213fd
lecopivo <skrivantomas@seznam.cz>
2024-03-06 09:52:01 -0500

    autodiff tactic

---
+++ SciLean/Tactic/Autodiff.lean
@@ -0,0 +1,18 @@
+import SciLean.Core.FunctionTransformations
+import SciLean.Tactic.LetNormalize2
+
+namespace SciLean.Tactic
+
+open Lean Meta
+simproc_decl lift_lets_simproc (_) := fun e => do
+  let E ← inferType e
+  if (← Meta.isClass? E).isSome then return .continue
+  let e ← e.liftLets2 (fun xs b => do
+      if xs.size ≠ 0
+      then mkLetFVars xs (← Simp.dsimp b)
+      else pure b) {}
+  return .visit {expr:=e}
+
+
+macro "autodiff" : conv =>
+  `(conv| fun_trans (config:={zeta:=false,singlePass:=true}) (disch:=sorry) only [ftrans_simp,lift_lets_simproc])
