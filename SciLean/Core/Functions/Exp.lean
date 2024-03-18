50411aa7e8b017ae12caabdc814dba0cbb71991e
lecopivo <skrivantomas@seznam.cz>
2024-03-18 10:01:03 -0400

    update on properties of exp, sin, cos

--- SciLean/Core/Functions/Exp.lean
+++ SciLean/Core/Functions/Exp.lean
@@ -1,12 +1,12 @@
 import SciLean.Core.FunctionTransformations
-import SciLean.Core.Meta.GenerateRevDeriv
+-- import SciLean.Core.Meta.GenerateRevDeriv
 
 open ComplexConjugate
 
 namespace SciLean.Scalar
 
 variable
-  {R C} [Scalar R C]
+  {R C} [Scalar R C] [RealScalar R]
   {W} [Vec C W]
   {U} [SemiInnerProductSpace C U]
 
@@ -15,68 +15,158 @@ variable
 -- Exp -------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[fprop]
-theorem exp.arg_x.IsDifferentiable_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : IsDifferentiable C fun w => exp (x w) := sorry_proof
+@[fun_prop]
+theorem exp.arg_x.CDifferentiable_rule
+  (x : W → C) (hx : CDifferentiable C x)
+  : CDifferentiable C fun w => exp (x w) := sorry_proof
 
-@[ftrans]
+@[fun_trans]
 theorem exp.arg_x.ceriv_rule
-  (x : W → C) (hx : IsDifferentiable C x)
+  (x : W → C) (hx : CDifferentiable C x)
   : cderiv C (fun w => exp (x w))
     =
     fun w dw =>
-      let xdx := fwdCDeriv C x w dw
+      let xdx := fwdDeriv C x w dw
       let e := exp xdx.1
       xdx.2 * e := sorry_proof
 
-@[ftrans]
-theorem exp.arg_x.fwdCDeriv_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : fwdCDeriv C (fun w => exp (x w))
+@[fun_trans]
+theorem exp.arg_x.fwdDeriv_rule
+    (x : W → C) (hx : CDifferentiable C x) :
+    fwdDeriv C (fun w => exp (x w))
     =
     fun w dw =>
-      let xdx := fwdCDeriv C x w dw
+      let xdx := fwdDeriv C x w dw
       let e := exp xdx.1
-      (e, xdx.2 * e) :=
-by
-  unfold fwdCDeriv; ftrans; rfl
+      (e, xdx.2 * e) := by
+  unfold fwdDeriv; fun_trans; rfl
 
-#generate_revDeriv exp x
-  prop_by unfold HasAdjDiff; constructor; fprop; ftrans; fprop
-  abbrev trans_by unfold revDeriv; ftrans; ftrans
+@[fun_prop]
+theorem exp.arg_x.HasAdjDiff_rule
+    (x : U → C) (hx : HasAdjDiff C x) :
+    HasAdjDiff C (fun u => exp (x u)) := by
+  intro x
+  constructor
+  fun_prop
+  fun_trans [fwdDeriv]; fun_prop
+
+open ComplexConjugate
+@[fun_trans]
+theorem exp.arg_x.revDeriv_rule
+    (x : U → C) (hx : HasAdjDiff C x) :
+    revDeriv C (fun u => exp (x u))
+    =
+    fun u =>
+      let xdx := revDeriv C x u
+      (exp xdx.1, fun dy => xdx.2 (conj (exp xdx.1) * dy)) := by
+  unfold revDeriv
+  fun_trans only [fwdDeriv, smul_push, ftrans_simp]
+
 
 
 --------------------------------------------------------------------------------
 -- Log -------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[fprop]
-theorem log.arg_x.IsDifferentiable_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : IsDifferentiable C fun w => log (x w) := sorry_proof
+section Log
 
-@[ftrans]
-theorem log.arg_x.ceriv_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : cderiv C (fun w => log (x w))
+variable
+  {R : Type _} [RealScalar R]
+  {W : Type _} [Vec R W]
+  {U : Type _} [SemiInnerProductSpace R U]
+
+
+@[fun_prop]
+theorem log.arg_x.CDifferentiableAt_rule
+    (w : W) (x : W → R) (hx : CDifferentiableAt R x w) (hw : x w ≠ 0) :
+    CDifferentiableAt R (fun w => log (x w)) w := sorry_proof
+
+@[fun_prop]
+theorem log.arg_x.CDifferentiable_rule
+    (x : W → R) (hx : CDifferentiable R x) (hw : ∀ w, x w ≠ 0) :
+    CDifferentiable R (fun w => log (x w)) := by
+  intro x; fun_prop (disch:=aesop)
+
+@[fun_trans]
+theorem log.arg_x.cderiv_rule_at
+    (w : W) (x : W → R) (hx : CDifferentiableAt R x w) (hw : x w ≠ 0) :
+    cderiv R (fun w => log (x w)) w
+    =
+    fun dw =>
+      let xdx := fwdDeriv R x w dw
+      xdx.2 / abs xdx.1 := sorry_proof
+
+@[fun_trans]
+theorem log.arg_x.cderiv_rule
+    (x : W → R) (hx : CDifferentiable R x) (hw : ∀ w, x w ≠ 0) :
+    cderiv R (fun w => log (x w))
     =
     fun w dw =>
-      let xdx := fwdCDeriv C x w dw
-      xdx.2 / xdx.1 := sorry_proof
+      let xdx := fwdDeriv R x w dw
+      xdx.2 / abs xdx.1 := by
+  funext x
+  fun_trans (disch:=aesop)
 
-@[ftrans]
-theorem log.arg_x.fwdCDeriv_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : fwdCDeriv C (fun w => log (x w))
+@[fun_trans]
+theorem log.arg_x.fwdDeriv_rule_at
+    (w : W) (x : W → R) (hx : CDifferentiableAt R x w) (hw : x w ≠ 0) :
+    fwdDeriv R (fun w => log (x w)) w
     =
-    fun w dw =>
-      let xdx := fwdCDeriv C x w dw
+    fun dw =>
+      let xdx := fwdDeriv R x w dw
       let l := log xdx.1
-      (l, xdx.2 / xdx.1) :=
+      (l, xdx.2 / abs xdx.1) :=
 by
-  unfold fwdCDeriv; ftrans; simp[fwdCDeriv]
+  unfold fwdDeriv; fun_trans (disch:=assumption); simp[fwdDeriv]
 
-#generate_revDeriv log x
-  prop_by unfold HasAdjDiff; constructor; fprop; ftrans; fprop
-  abbrev trans_by unfold revDeriv; ftrans; ftrans
+@[fun_trans]
+theorem log.arg_x.fwdDeriv_rule
+    (x : W → R) (hx : CDifferentiable R x) (hw : ∀ w, x w ≠ 0) :
+    fwdDeriv R (fun w => log (x w))
+    =
+    fun w dw =>
+      let xdx := fwdDeriv R x w dw
+      let l := log xdx.1
+      (l, xdx.2 / abs xdx.1) :=
+by
+  unfold fwdDeriv; fun_trans (disch:=assumption); simp[fwdDeriv]
+
+@[fun_prop]
+theorem log.arg_x.HasAdjDiffAt_rule
+    (u : U) (x : U → R) (hx : HasAdjDiffAt R x u) (hu : x u ≠ 0) :
+    HasAdjDiffAt R (fun u => log (x u)) u := by
+  constructor
+  fun_prop (disch:=aesop)
+  fun_trans (disch:=aesop) [fwdDeriv]; fun_prop
+
+@[fun_prop]
+theorem log.arg_x.HasAdjDiff_rule
+    (x : U → R) (hx : HasAdjDiff R x) (hu : ∀ u, x u ≠ 0) :
+    HasAdjDiff R (fun u => log (x u)) := by
+  intro u;
+  fun_prop (disch:=aesop)
+
+open ComplexConjugate
+@[fun_trans]
+theorem log.arg_x.revDeriv_rule_at
+    (u : U) (x : U → R) (hx : HasAdjDiffAt R x u) (hu : x u ≠ 0) :
+    revDeriv R (fun u => log (x u)) u
+    =
+    let xdx := revDeriv R x u
+    (log xdx.1, fun dy => xdx.2 ((abs (x u))⁻¹ * dy)) := by
+  unfold revDeriv
+  fun_trans (disch:=aesop) only [fwdDeriv, smul_push, ftrans_simp]
+
+open ComplexConjugate
+@[fun_trans]
+theorem log.arg_x.revDeriv_rule
+    (x : U → R) (hx : HasAdjDiff R x) (hu : ∀ u, x u ≠ 0) :
+    revDeriv R (fun u => log (x u))
+    =
+    fun u =>
+      let xdx := revDeriv R x u
+      (log xdx.1, fun dy => xdx.2 ((abs (x u))⁻¹ * dy)) := by
+  unfold revDeriv
+  fun_trans (disch:=aesop) only [fwdDeriv, smul_push, ftrans_simp]
+
+end Log
--- SciLean/Core/Functions/Trigonometric.lean
+++ SciLean/Core/Functions/Trigonometric.lean
@@ -1,5 +1,5 @@
 import SciLean.Core.FunctionTransformations
-import SciLean.Core.Meta.GenerateRevDeriv
+-- import SciLean.Core.Meta.GenerateRevDeriv
 
 open ComplexConjugate
 
@@ -15,116 +15,164 @@ variable
 -- Sin -------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[fprop]
-theorem sin.arg_x.IsDifferentiable_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : IsDifferentiable C fun w => sin (x w) := sorry_proof
+@[fun_prop]
+theorem sin.arg_x.CDifferentiable_rule
+    (x : W → C) (hx : CDifferentiable C x) :
+    CDifferentiable C fun w => sin (x w) := sorry_proof
 
-@[ftrans]
+@[fun_trans]
 theorem sin.arg_x.ceriv_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : cderiv C (fun w => sin (x w))
+    (x : W → C) (hx : CDifferentiable C x) :
+    cderiv C (fun w => sin (x w))
     =
     fun w dw =>
-      let xdx := fwdCDeriv C x w dw
+      let xdx := fwdDeriv C x w dw
       let c := cos xdx.1
       xdx.2 * c := sorry_proof
 
-@[ftrans]
-theorem sin.arg_x.fwdCDeriv_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : fwdCDeriv C (fun w => sin (x w))
+@[fun_trans]
+theorem sin.arg_x.fwdDeriv_rule
+    (x : W → C) (hx : CDifferentiable C x) :
+    fwdDeriv C (fun w => sin (x w))
     =
     fun w dw =>
-      let xdx := fwdCDeriv C x w dw
+      let xdx := fwdDeriv C x w dw
       let s := sin xdx.1
       let c := cos xdx.1
-      (s, xdx.2 * c) :=
-by
-  unfold fwdCDeriv; ftrans; rfl
+      (s, xdx.2 * c) := by
+  unfold fwdDeriv; fun_trans; rfl
 
-#generate_revDeriv sin x
-  prop_by unfold HasAdjDiff; constructor; fprop; ftrans; fprop
-  abbrev trans_by unfold revDeriv; ftrans; ftrans
+
+@[fun_trans]
+theorem sin.arg_x.HasAdjDiff_rule
+    (x : U → C) (hx : HasAdjDiff C x) :
+    HasAdjDiff C (fun u => sin (x u)) := by
+  intro x
+  constructor
+  fun_prop
+  fun_trans [fwdDeriv]; fun_prop
+
+
+open ComplexConjugate
+@[fun_trans]
+theorem sin.arg_x.revDeriv_rule
+    (x : U → C) (hx : HasAdjDiff C x) :
+    revDeriv C (fun u => sin (x u))
+    =
+    fun u =>
+      let xdx := revDeriv C x u
+      (sin xdx.1, fun dy => xdx.2 (conj (cos xdx.1) * dy)) := by
+  unfold revDeriv
+  fun_trans only [fwdDeriv, smul_push, ftrans_simp]
 
 
 --------------------------------------------------------------------------------
 -- Cos -------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[fprop]
-theorem cos.arg_x.IsDifferentiable_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : IsDifferentiable C fun w => cos (x w) := sorry_proof
+@[fun_prop]
+theorem cos.arg_x.CDifferentiable_rule
+  (x : W → C) (hx : CDifferentiable C x)
+  : CDifferentiable C fun w => cos (x w) := sorry_proof
 
-@[ftrans]
+@[fun_trans]
 theorem cos.arg_x.ceriv_rule
-  (x : W → C) (hx : IsDifferentiable C x)
+  (x : W → C) (hx : CDifferentiable C x)
   : cderiv C (fun w => cos (x w))
     =
     fun w dw =>
-      let xdx := fwdCDeriv C x w dw
+      let xdx := fwdDeriv C x w dw
       let s := -sin xdx.1
       xdx.2 * s := sorry_proof
 
-@[ftrans]
-theorem cos.arg_x.fwdCDeriv_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : fwdCDeriv C (fun w => cos (x w))
+@[fun_trans]
+theorem cos.arg_x.fwdDeriv_rule
+  (x : W → C) (hx : CDifferentiable C x)
+  : fwdDeriv C (fun w => cos (x w))
     =
     fun w dw =>
-      let xdx := fwdCDeriv C x w dw
+      let xdx := fwdDeriv C x w dw
       let s := sin xdx.1
       let c := cos xdx.1
       (c, xdx.2 * -s) :=
 by
-  unfold fwdCDeriv; ftrans; rfl
+  unfold fwdDeriv; fun_trans; rfl
 
-#generate_revDeriv cos x
-  prop_by unfold HasAdjDiff; constructor; fprop; ftrans; fprop
-  abbrev trans_by unfold revDeriv; ftrans; ftrans
+@[fun_trans]
+theorem cos.arg_x.HasAdjDiff_rule
+    (x : U → C) (hx : HasAdjDiff C x) :
+    HasAdjDiff C (fun u => cos (x u)) := by
+  intro x
+  constructor
+  fun_prop
+  fun_trans [fwdDeriv]; fun_prop
+
+
+open ComplexConjugate
+@[fun_trans]
+theorem cos.arg_x.revDeriv_rule
+    (x : U → C) (hx : HasAdjDiff C x) :
+    revDeriv C (fun u => cos (x u))
+    =
+    fun u =>
+      let xdx := revDeriv C x u
+      (cos xdx.1, fun dy => xdx.2 (conj (- sin xdx.1) * dy)) := by
+  unfold revDeriv
+  fun_trans only [fwdDeriv, smul_push, ftrans_simp]
 
 
 --------------------------------------------------------------------------------
--- Tang -------------------------------------------------------------------------
+-- Tanh -------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-@[fprop]
-theorem tanh.arg_x.IsDifferentiable_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : IsDifferentiable C fun w => tanh (x w) := sorry_proof
+@[fun_prop]
+theorem tanh.arg_x.CDifferentiable_rule
+    (x : W → C) (hx : CDifferentiable C x) :
+    CDifferentiable C fun w => tanh (x w) := sorry_proof
 
-@[ftrans]
+@[fun_trans]
 theorem tanh.arg_x.ceriv_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : cderiv C (fun w => tanh (x w))
+    (x : W → C) (hx : CDifferentiable C x) :
+    cderiv C (fun w => tanh (x w))
     =
     fun w dw =>
-      let xdx := fwdCDeriv C x w dw
+      let xdx := fwdDeriv C x w dw
       let t := tanh xdx.1
       let dt := 1 - t^2
       xdx.2 * dt := sorry_proof
 
-@[ftrans]
-theorem tanh.arg_x.fwdCDeriv_rule
-  (x : W → C) (hx : IsDifferentiable C x)
-  : fwdCDeriv C (fun w => tanh (x w))
+@[fun_trans]
+theorem tanh.arg_x.fwdDeriv_rule
+    (x : W → C) (hx : CDifferentiable C x) :
+    fwdDeriv C (fun w => tanh (x w))
     =
     fun w dw =>
-      let xdx := fwdCDeriv C x w dw
+      let xdx := fwdDeriv C x w dw
       let t := tanh xdx.1
       let dt := 1-t^2
       (t, xdx.2 * dt) :=
 by
-  unfold fwdCDeriv; ftrans; rfl
+  unfold fwdDeriv; fun_trans; rfl
 
-#generate_revDeriv tanh x
-  prop_by unfold HasAdjDiff; constructor; fprop; ftrans; fprop
-  abbrev trans_by
-    unfold revDeriv; ftrans; ftrans
-    enter [x]
-    -- we just need to replace `tanh x` with `t`, there should be a tactic for it
-    -- or common subexpression optimization should do it
-    equals (let t := tanh x;
-            let dt := 1 - t ^ 2;
-            (t, fun y => (starRingEnd K) dt * y)) => rfl
+
+@[fun_trans]
+theorem tanh.arg_x.HasAdjDiff_rule
+    (x : U → C) (hx : HasAdjDiff C x) :
+    HasAdjDiff C (fun u => tanh (x u)) := by
+  intro x
+  constructor
+  fun_prop
+  fun_trans [fwdDeriv]; fun_prop
+
+
+open ComplexConjugate
+@[fun_trans]
+theorem tanh.arg_x.revDeriv_rule
+    (x : U → C) (hx : HasAdjDiff C x) :
+    revDeriv C (fun u => tanh (x u))
+    =
+    fun u =>
+      let xdx := revDeriv C x u
+      (tanh xdx.1, fun dy => xdx.2 (conj (1 - tanh (xdx.1) ^ 2) * dy)) := by
+  unfold revDeriv
+  fun_trans only [fwdDeriv, smul_push, ftrans_simp]
