193fdb161c47eaa022ee27ea1b8a43e44a7f9ee4
lecopivo <skrivantomas@seznam.cz>
2024-03-27 00:52:02 +0100

    ...

--- SciLean/Core/Distribution/SimpleExamples2D.lean
+++ SciLean/Core/Distribution/SimpleExamples2D.lean
@@ -4,6 +4,9 @@ import SciLean.Core.Distribution.Eval
 import SciLean.Core.Integral.Substitution
 import SciLean.Core.Integral.ParametricInverse
 import SciLean.Core.Integral.Frontier
+
+import SciLean.Core.FunctionTransformations.Preimage
+
 import SciLean.Core
 
 import SciLean.Util.RewriteBy
@@ -23,24 +26,13 @@ set_default_scalar R
 
 attribute [ftrans_simp]
   FiniteDimensional.finrank_self FiniteDimensional.finrank_prod
-  mem_setOf_eq not_le ite_mul mul_ite mul_neg mul_one setOf_eq_eq_singleton mem_Ioo
+  not_le ite_mul mul_ite mul_neg mul_one setOf_eq_eq_singleton
   Finset.card_singleton PUnit.default_eq_unit Finset.univ_unique Finset.sum_const
   preimage_id'
-  mem_ite_empty_right
+  mem_setOf_eq mem_ite_empty_right mem_inter_iff mem_ite_empty_right mem_univ mem_Ioo
+  and_true
 
 
-open Classical
-@[simp, ftrans_simp]
-theorem hoho {α β γ} (f : α → β) (c : γ) (B : Set β) (C : Set γ) :
-  (fun x => (f x, c)) ⁻¹' (B.prod C) = if c ∈ C then f ⁻¹' B else ∅ := sorry_proof
-
-
-@[simp, ftrans_simp]
-theorem hoho' {α β γ} (f : α → β) (c : γ) (B : Set β) (C : Set γ) :
-  (fun x => (c, f x)) ⁻¹' (C.prod B) = if c ∈ C then f ⁻¹' B else ∅ := sorry_proof
-
--- set_option trace.Meta.Tactic.simp.unify true in
--- set_option trace.Meta.Tactic.simp.discharge true in
 def foo1 (t' : R) :=
   derive_random_approx
     (∂ (t:=t'), ∫' (xy : R×R) in (Ioo 0 1).prod (Ioo 0 1), if xy.1 ≤ t then (1:R) else 0)
@@ -61,7 +53,6 @@ def foo1 (t' : R) :=
 
     simp (disch:=sorry) only [ftrans_simp]
     rand_pull_E
-
 
 #eval Rand.print_mean_variance (foo1 0.3) 100 ""
 
@@ -89,18 +80,46 @@ def foo2 (t' : R) :=
 
     autodiff; autodiff; norm_num
 
-    conv in Set.preimage _ _ =>
-      rw[hoho']
-      simp only [ftrans_simp]
-
     conv in Set.preimage1 _ _ =>
       rw[Set.preimage1_prod]
       simp only [ftrans_simp]
 
-    simp only [ftrans_simp,action_push,hoho,hoho']
+    simp only [ftrans_simp,action_push]
     simp (disch:=sorry) only [ftrans_simp]
     rand_pull_E
 
 
 #eval Rand.print_mean_variance (foo2 0.3) 1000 ""
 #eval Rand.print_mean_variance (foo2 1.7) 1000 ""
+
+
+def foo3 (t' : R) :=
+  derive_random_approx
+    (∂ (t:=t'), ∫' (xy : R×R) in (Ioo 0 1).prod (Ioo 0 1), if xy.1 + xy.2 ≤ t then (1:R) else 0)
+  by
+    fun_trans only [scalarGradient, scalarCDeriv]
+    simp only [ftrans_simp]
+
+    rw[surfaceDirac_substitution
+        (I:= Unit)
+        (X₁:=fun _ => R) (X₂:= fun _ => R)
+        (p:= fun _ x y => (x+y,y-x))
+        (ζ:= fun b _ => t'/2)
+        (dom:= fun _ => Set.univ)
+        (inv:= by intro i x₁ _; dsimp; ring) (hdim := sorry_proof)]
+
+    autodiff; autodiff; norm_num only [ftrans_simp]
+
+    conv in Set.preimage1 _ _ =>
+      unfold Set.preimage1
+      simp only [ftrans_simp]
+      fun_trans
+      equals Ioo (-1) 1 => sorry
+
+    simp only [ftrans_simp,action_push]
+    simp (disch:=sorry) only [ftrans_simp]
+    norm_num only [ftrans_simp]
+    rand_pull_E
+
+#eval Rand.print_mean_variance (foo3 0.3) 10000 ""
+#eval Rand.print_mean_variance (foo3 1.7) 10000 ""
