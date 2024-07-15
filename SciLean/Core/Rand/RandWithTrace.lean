e773bcf458e3d4c422a562b3fbd952d660114af1
lecopivo <skrivantomas@seznam.cz>
2024-07-15 09:46:26 -0400

    update RandWithTrace monad

--- SciLean/Core/Rand/RandWithTrace.lean
+++ SciLean/Core/Rand/RandWithTrace.lean
@@ -9,55 +9,76 @@ set_option linter.unusedVariables false
 namespace SciLean.Rand
 
 inductive Trace where
-| list (l : List (Name×Type))
+| nil
+| cons (tag : Name) (T : Type) (tail : Trace)
 
-def Trace.append (t s : Trace) : Trace :=
+protected def Trace.append : (t s : Trace) → Trace
+  | .nil,    s => s
+  | .cons n T ts, s => .cons n T (Trace.append ts s)
+
+instance : Append Trace := ⟨fun t s => t.append s⟩
+
+-- open Classical in
+def Trace.type : (trace : Trace) → Type
+  | .nil => Unit
+  -- | .cons n T .nil => T -- this case would be nice but makes all the proofs more compilicated
+  | .cons n T tr => T × tr.type
+
+def Trace.tags : (trace : Trace) → List Name
+  | .nil => []
+  | .cons n _ tr => n :: tr.tags
+
+
+@[inline]
+def Trace.appendIso {t s : Trace} : (t.append s).type ≃ t.type × s.type :=
   match t, s with
-  | .list t, .list s => .list (t ++ s)
-
-open Classical in
-def Trace.type (trace : Trace) : Type :=
-  match trace with
-  | .list [] => Unit
-  | .list [(_,T)] => T
-  | .list ((_,t)::tr) => t × (Trace.list tr).type
-
-def Trace.tags (trace : Trace) : List Name :=
-  match trace with
-  | .list [] => []
-  | .list ((n,_)::tr) => n :: (Trace.list tr).tags
-
-def Trace.appendIso {t s : Trace} : (t.append s).type ≃ t.type × s.type := sorry
+  | .nil, s => {
+    toFun := fun x => ((),x)
+    invFun := fun (_,x) => x
+    left_inv := by intro x; rfl
+    right_inv := by intro x; rfl
+  }
+  | .cons n T tr, s => {
+    toFun := fun (x,y) => let (a,b) := appendIso y; ((x,a),b)
+    invFun := fun ((x,x'),y) => (x, appendIso.symm (x',y))
+    left_inv := by intro (x,y); simp
+    right_inv := by intro ((x,y),z); simp
+  }
 
 structure RandWithTrace (X : Type) (trace : Trace) where
   rand : Rand X
-  r : Rand trace.type
+  traceRand : Rand trace.type
   map : trace.type → X
+  consistent : traceRand.map map = rand
 
-def return' (x : X) : RandWithTrace X (.list []) where
+def return' (x : X) : RandWithTrace X .nil where
   rand := Pure.pure x
-  r := Pure.pure (Eq.mp (by simp[Trace.type]) ())
+  traceRand := Pure.pure ()
   map := fun _ => x
+  consistent := by simp
 
 def RandWithTrace.bind (x : RandWithTrace X t) (f : X → RandWithTrace Y s)
     (hinter : t.tags.inter s.tags = [] := by simp[Trace.tags,List.inter]; done)
-    {tr} (hu : tr = t.append s := by simp[Trace.append]; infer_var) : RandWithTrace Y tr where
-  r := do
-    let xr ← x.r
-    let x := x.map (← x.r)
-    let yr ← (f x).r
-    let t := Trace.appendIso.symm (xr,yr)
-    pure (hu ▸ t)
+    {tr} (hu : tr = t.append s := by simp[Trace.append,Append.append]; infer_var) : RandWithTrace Y tr where
   rand := x.rand >>= (fun x' => (f x').rand)
+  traceRand := do
+    let tx ← x.traceRand
+    let x := x.map tx
+    let ty ← (f x).traceRand
+    let t := Trace.appendIso.symm (tx,ty)
+    pure (hu ▸ t)
   map s :=
     let (sx,sy) := Trace.appendIso (hu▸s)
     (f (x.map sx)).map sy
+  consistent := sorry_proof
 
 def sample {X} (x : Rand X) (tag : Name) :
-    RandWithTrace X (.list [(tag,X)]) where
+    RandWithTrace X (.cons tag X .nil) where
   rand := x
-  r := Eq.mp (by simp[Trace.type]) x
-  map := fun x => (x rewrite_type_by simp[Trace.type])
+  traceRand := do let x ← x; pure (x,())
+  map := fun (x,_) => x
+  consistent := by simp
+
 
 open Lean.Parser Term in
 syntax (name:=probStx) withPosition("let" funBinder " <~ " term (semicolonOrLinebreak ppDedent(ppLine) term)?) : term
@@ -94,33 +115,28 @@ unsafe def probStxElab : TermElab := fun stx _ =>
   | _ => throwUnsupportedSyntax
 
 
+abbrev RandWithTrace.traceType (x : RandWithTrace X tr) {T} (h : T = tr.type := by simp[Trace.append,Trace.type]; infer_var) := T
+
+def RandWithTrace.map' (x : RandWithTrace X tr) {T} (h : T = tr.type := by simp[Trace.append,Trace.type]; infer_var) := fun t : T => x.map (h▸t)
+
+def RandWithTrace.traceRand' (x : RandWithTrace X tr) {T} (h : T = tr.type := by simp[Trace.append,Trace.type]; infer_var) := do
+  let t ← x.traceRand
+  return (Eq.mpr h t)
+
+def RandWithTrace.get (x : RandWithTrace X tr) := x.rand.get
+def RandWithTrace.getTrace (x : RandWithTrace X tr) {T} (h : T = tr.type := by simp[Trace.append,Trace.type]; infer_var) := (x.traceRand' h).get
+
 def test1 :=
   let x <~ sample (flip 0.5) `v1
-  let y <~ sample (normal 0.1 0.3) `v2
-  let z <~ sample (uniform (Set.Icc 0.1 0.3)) `v3
-  return' (x,y,z)
+  let y <~ sample (normal 0.1 (x.toNat.toFloat)) `v2
+  let z <~ sample (uniform (Set.Icc y (2*y))) `v3
+  return' (x.toNat.toFloat+y+z)
 
-def test2 :=
-  let x <~ sample (flip 0.5) `a
-  let y <~ sample (normal 0.1 0.3) `b
-  let z <~ sample (uniform (Set.Icc 0.1 0.3)) `c
-  return' (x,y,z)
 
-def test3 :=
-  let t1 <~ test1
-  let t2 <~ test2
-  return' (t1+t2)
+#eval test1.rand.get
+#eval test1.traceRand'.get
+#check test1.map'
 
-#check test1
-#check test2
-#check test3
 
--- def invalid1 :=
---   let x <~ sample (flip 0.5) `a
---   let y <~ sample (normal 0.1 0.3) `a
---   return' (x,y)
-
--- def invalid2 :=
---   let x <~ test1
---   let y <~ test1
---   return' (x+y)
+#eval print_mean_variance test1.rand 1000 ""
+#eval print_mean_variance (do let tr ← test1.traceRand; return test1.map tr) 1000 ""
