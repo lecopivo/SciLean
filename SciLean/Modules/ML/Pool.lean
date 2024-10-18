877dc910b22cc33c919d1774cbada597199167c8
lecopivo <skrivantomas@seznam.cz>
2024-10-18 12:08:26 +0200

    update pool layer

--- SciLean/Modules/ML/Pool.lean
+++ SciLean/Modules/ML/Pool.lean
@@ -1,43 +1,77 @@
-import SciLean.Core
-import SciLean.Core.Functions.Exp
-import SciLean.Data.DataArray
-import SciLean.Data.ArrayType
-import SciLean.Data.Prod
-import Mathlib
+import SciLean
 
 namespace SciLean.ML
-
-set_option synthInstance.maxSize 200000
 
 variable
   {R : Type} [RealScalar R] [PlainDataType R]
 
 set_default_scalar R
 
+@[simp_core ↑]
+theorem Function.uncurry.arg_f.revFDerivProj_rule_1
+  {I₁} [IndexType I₁]
+  {R} [RCLike R]
+  {X} [NormedAddCommGroup X] [AdjointSpace R X] :
+  revFDerivProj R Unit (fun (f : I₁ → X) => ↿f)
+  =
+  fun f =>
+    (↿f, fun _ df i₁ => df i₁) := sorry_proof
 
-def _root_.SciLean.Idx.prodMerge (i : Idx (n/m)) (j : Idx m) : Idx n := ⟨i.1 * m + j.1, sorry_proof⟩
-def _root_.SciLean.Idx.prodMerge' (i : Idx (n/m)) (j : Idx m) : Idx n := ⟨i.1 + j.1 * (n/m), sorry_proof⟩
+@[simp_core ↑]
+theorem Function.uncurry.arg_f.revFDerivProj_rule_2
+  {I₁} [IndexType I₁]
+  {I₂} [IndexType I₂]
+  {R} [RCLike R]
+  {X} [NormedAddCommGroup X] [AdjointSpace R X] :
+  revFDerivProj R Unit (fun (f : I₁ → I₂ → X) => ↿f)
+  =
+  fun f =>
+    (↿f, fun _ df i₁ i₂ => df (i₁,i₂)) := sorry_proof
+
+@[simp_core ↑]
+theorem Function.uncurry.arg_f.revFDerivProj_rule_3
+  {I₁} [IndexType I₁]
+  {I₂} [IndexType I₂]
+  {I₃} [IndexType I₃]
+  {R} [RCLike R]
+  {X} [NormedAddCommGroup X] [AdjointSpace R X] :
+  revFDerivProj R Unit (fun (f : I₁ → I₂ → I₃ → X) => ↿f)
+  =
+  fun f =>
+    (↿f, fun _ df i₁ i₂ i₃ => df (i₁,i₂,i₃)) := sorry_proof
 
 
-def avgPool {n m : USize} {ι} [Index ι] (x : R^[ι,n,m]) : R^[ι,n/2,m/2] :=
-  ⊞ (k,i,j) => (1/4 : R) * ∑ i' j' : Idx 2, x[(k,i.prodMerge i', j.prodMerge j')]
-
-#generate_revDeriv avgPool x
-  prop_by unfold avgPool; fprop
-  trans_by unfold avgPool; ftrans
-
-
-def softMaxPool {n m : USize} {ι} [Index ι] (scale : R) (x : R^[ι,n,m]) : R^[ι,n/2,m/2] :=
-  introElem fun (k,i,j) =>
-    let ex := ⊞ (ij : Idx 2 × Idx 2) => Scalar.exp (scale*x[(k, i.prodMerge ij.1, j.prodMerge ij.2)])
-    let w := ∑ i', ex[i']
-    let w' := 1/w
-    ∑ i' j' : Idx 2,
-      let xi := x[(k, i.prodMerge i', j.prodMerge j')]
-      let exi := ex[(i',j')]
-      xi * (w'*exi)
+open SciLean
+variable {n₁ n₂ : Nat} {I : Type} [IndexType I] [DecidableEq I]
+def avgPool2d {R : Type} [RealScalar R] [PlainDataType R]
+    (x : R^[I,n₁,n₂]) {m₁ m₂ : Nat}
+    (h₁ : m₁ = n₁/2 := by infer_var)
+    (h₂ : m₂ = n₂/2 := by infer_var) : R^[I,m₁,m₂] :=
+  ⊞ (ι : I) (i : Fin m₁) (j : Fin m₂) =>
+    let i₁ : Fin n₁ := ⟨2*i.1, by omega⟩
+    let i₂ : Fin n₁ := ⟨2*i.1+1, by omega⟩
+    let j₁ : Fin n₂ := ⟨2*j.1, by omega⟩
+    let j₂ : Fin n₂ := ⟨2*j.1+1, by omega⟩
+    (x[ι,i₁,j₁] + x[ι,i₁,j₂] + x[ι,i₂,j₁] + x[ι,i₂,j₂])
 
 
-#generate_revDeriv softMaxPool x
-  prop_by unfold softMaxPool; fprop
-  trans_by unfold softMaxPool; ftrans
+def_fun_prop avgPool2d in x
+  : Differentiable R
+
+def_fun_trans avgPool2d in x
+  add_suffix _unit
+  [DecidableEq I] : revFDerivProj R Unit by unfold avgPool2d; autodiff
+
+def_fun_trans avgPool2d in x
+  add_suffix _unit
+  [DecidableEq I] : revFDerivProjUpdate R Unit by unfold avgPool2d; autodiff
+
+def_fun_trans avgPool2d in x
+  [DecidableEq I] : revFDerivProj R ((I×Fin m₁×Fin m₂)×Unit) by unfold avgPool2d; autodiff
+
+def_fun_trans avgPool2d in x
+  [DecidableEq I] : revFDerivProjUpdate R ((I×Fin m₁×Fin m₂)×Unit) by unfold avgPool2d; autodiff
+
+
+def_fun_trans avgPool2d in x
+  [DecidableEq I] : revFDeriv R by rw[revFDeriv_eq_revFDerivProj]; autodiff
