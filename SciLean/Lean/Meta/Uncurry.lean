1c9675917e30e6e8eca363acc770de4ba78e6eb2
lecopivo <skrivantomas@seznam.cz>
2024-12-02 21:15:29 -0500

    meta functions to uncurry function

---
+++ SciLean/Lean/Meta/Uncurry.lean
@@ -0,0 +1,188 @@
+import Lean
+import Batteries.Lean.Expr
+
+import SciLean.Lean.Expr
+import SciLean.Lean.Array
+import SciLean.Lean.Meta.Basic
+import SciLean.Util.RewriteBy
+
+import Mathlib.Logic.Function.Basic
+
+namespace Lean.Meta
+
+
+/-- Returns `#[x.1, x.2.1, x.2.2.1, ..., x.2....2]` with `n` elements.
+Where `·.1` and `·.2` are `Prod` projections. -/
+def mkProdSplitElem' (x : Expr) (n : Nat) : Array Expr :=
+  if n = 0 then
+    #[]
+  else if n = 1 then
+    #[x]
+  else
+    go n x #[]
+where
+  go (n : Nat) (x : Expr) (xs : Array Expr) : Array Expr :=
+    match n with
+    | 0 => xs
+    | 1 => xs.push x
+    | n+1 => go n (x.proj ``Prod 1) (xs.push (x.proj ``Prod 0))
+
+
+/-- Takes expression `b` with free vars `xs = #[x₁, ..., xₙ]` and returns lambda function in one
+argument of the form:
+```
+fun x =>
+  let x₁ := x.1
+  let x₂ := x.2.1
+  ...
+  let xₙ := x.2....2
+  b
+``` -/
+def mkUncurryLambdaFVars (xs : Array Expr) (b : Expr) : MetaM Expr := do
+
+  if xs.size = 1 then return ← mkLambdaFVars xs b
+
+  let x ← mkProdElem xs
+  let X ← inferType x
+
+  let xnames ← xs.mapM (fun x => x.fvarId!.getUserName)
+
+  withLocalDeclD `x X fun xvar => do
+
+    let xvals := mkProdSplitElem' xvar xs.size
+
+    -- tag values with meta-data
+    -- let mdata := MData.empty |>.set `uncurry_fun true |>.set `uncurry_fun_size n
+    -- let xvals := xvals.mapIdx (fun i xval =>
+    --   Expr.mdata (mdata.set `uncurry_fun_idx i) xval)
+
+    withLetDecls xnames xvals fun xvars => do
+      let b := b.replaceFVars xs xvars
+      mkLambdaFVars (#[xvar] ++ xvars) b
+
+
+/-- Takes function of `n` arguments and returns uncurried version of `f` in specific form:
+```
+fun x =>
+  let x₁ := x.1
+  let x₂ := x.2.1
+  ...
+  let xₙ := x.2....2
+  f x₁ ... xₙ
+``` -/
+def mkUncurryFun' (n : Nat) (f : Expr) : MetaM Expr := do
+  if n ≤ 1 then
+    return f
+  else
+    lambdaBoundedTelescope f n fun xs b => do
+      mkUncurryLambdaFVars xs b
+
+
+private def _root_.Lean.Expr.isProjOf (x y : Expr) : Bool :=
+  if (x == (.proj ``Prod 0 y)) ||
+     (x == (.proj ``Prod 1 y)) then
+    true
+  else
+    match x with
+    | .proj ``Prod _ x' => isProjOf x' y
+    | _ => false
+
+
+private partial def uncurryBodyTelescope (e : Expr) (x : Expr) (k : Array Expr → Expr → MetaM α) : MetaM α := do
+  go e x #[]
+where
+  go (b : Expr) (x' : Expr) (fvars : Array Expr) := do
+    match b with
+    | .letE n t xi b ndep => do
+
+      -- expected all nice, keep on going
+      -- let xi := x.2. ... .1
+      if xi == (.proj ``Prod 0 x') then
+        withLocalDeclD n t fun xivar => do
+          go (b.instantiate1 xivar) (.proj ``Prod 1 x') (fvars.push xivar)
+
+      -- terminating argument, we are done
+      -- let xi := x.2. ... .2
+      else if xi == x' then
+        if b.containsFVar x.fvarId! then
+          k #[x] e
+        else
+          withLocalDeclD n t fun xivar => do
+            let b := b.instantiate1 xivar
+            k (fvars.push xivar) b
+
+      -- looks like we skipped argument
+      -- let xi := x.2. ...  but not what we expected
+      else if xi.isProjOf x' then
+        let x' := (.proj ``Prod 1 x')
+        withLocalDeclD default (← inferType x') fun xivar => do
+          -- here we do not instantiate!
+          -- but call again with updated `x'`
+          go (.letE n t xi b ndep) x' (fvars.push xivar)
+
+      -- completely differente let binding
+      -- we are done with instantiating variables
+      else
+        let b := (.letE n t xi b ndep)
+        if b.containsFVar x.fvarId! then
+          k #[x] e
+        else
+          withLocalDeclD default (← inferType x') fun xivar => do
+            k (fvars.push xivar) b
+    | _ =>
+      if fvars.size = 0 then
+        k #[x] e
+      else
+        withLocalDeclD default (← inferType x') fun xivar => do
+          k (fvars.push xivar) b
+
+
+/-- Lamda telescope for uncurried functions. This version peels off only one lambda.
+
+For input function
+```
+fun x =>
+  let x₁ := x.1
+  let x₂ := x.2.1
+  ...
+  let xₙ := x.2....2
+  f x₁ ... xₙ
+```
+call
+```
+k #[y₁, ..., yₙ] (f y₁ ... yₙ)
+```
+where `yᵢ` are fresh free variables. -/
+def uncurryLambdaTelescopeOnceImpl [Inhabited α] (f : Expr) (k : Array Expr → Expr → MetaM α) : MetaM α := do
+  let .lam xname X b bi := f | throwError "lambda function expected in `uncurryLambdaTelescopeOnce`"
+
+  withLocalDecl xname bi X fun x => do
+    uncurryBodyTelescope (b.instantiate1 x) x k
+
+/-- Simproc that replaces `↿f` with `f` in uncurried lambda form. -/
+dsimproc_decl hasUncurryNormalize (↿_) := fun e => do
+
+  match e.getAppFnArgs with
+  | (``Function.HasUncurry.uncurry, #[F,_,Y,_,f]) =>
+    forallTelescope F fun Xs' Y' => do
+
+      -- right now we assume that function is always fully uncurried
+      -- TODO: generalize this
+      unless ← isDefEq Y Y' do return .continue
+      let arity := Xs'.size
+      return .visit (← mkUncurryFun' arity f)
+  | _ => return .continue
+
+
+/-- Simproc that replaces `Function.uncurry` with `f` in uncurried lambda form. -/
+dsimproc_decl uncurryNormalize (Function.uncurry _) := fun e => do
+
+  match e.getAppFnArgs with
+  | (``Function.uncurry, #[_,_,_,f]) =>
+      return .visit (← mkUncurryFun' 2 f)
+  | _ => return .continue
+
+
+
+#check (fun x => (↿(fun a b c d e : ℕ => a + e)) x) rewrite_by simp -zeta [hasUncurryNormalize]
+#check (fun x => (↿(fun a b c d e : ℕ => a + c)) x) rewrite_by simp -zeta [hasUncurryNormalize]
