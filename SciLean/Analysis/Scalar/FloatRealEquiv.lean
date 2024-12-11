54aaff7e3d537b36032fc50964707c56db2f4357
lecopivo <skrivantomas@seznam.cz>
2024-12-11 12:17:52 -0500

    turning real expressions to float

--- SciLean/Analysis/Scalar/FloatRealEquiv.lean
+++ SciLean/Analysis/Scalar/FloatRealEquiv.lean
@@ -1,4 +1,8 @@
 import Mathlib.Data.Real.Basic
+import Mathlib.AlgebraicGeometry.EllipticCurve.Weierstrass
+import Mathlib.AlgebraicGeometry.EllipticCurve.Projective
+
+import SciLean.Analysis.Scalar.FloatAsReal
 import SciLean.Tactic.DataSynth.Elab
 import SciLean.Tactic.DataSynth.Attr
 
@@ -44,6 +48,11 @@ instance [RealToFloatType R F] : RealToFloatType (Array R) (Array F) := ⟨⟩
 instance [RealToFloatType R F] : RealToFloatType (List R) (List F) := ⟨⟩
 
 
+@[data_synth out f]
+structure RealToFloatVal {R : Type} {F : outParam Type}
+  [RealToFloatType R F]
+  (r : R) (f : F) : Prop where
+
 @[data_synth out g in f]
 structure RealToFloatFun {R₁ R₂ : Type} {F₁ F₂ : outParam Type}
   [RealToFloatType R₁ F₁] [RealToFloatType R₂ F₂]
@@ -56,14 +65,16 @@ variable {R R₁ R₂ R₃ F F₁ F₂ F₃ : Type}
   [RealToFloatType R F] [RealToFloatType R₁ F₁] [RealToFloatType R₂ F₂] [RealToFloatType R₃ F₃]
 
 
-opaque realToFloatVal [Inhabited F] [RealToFloatType R F] : R → F
-
 @[data_synth]
 theorem id_rule : RealToFloatFun (fun x : R => x) (fun x : F => x) := ⟨⟩
-theorem const_rule [Inhabited F₂] (y : R₂) : RealToFloatFun (fun x : R₁ => y) (fun x : F₁ => realToFloatVal y) := ⟨⟩
+
+theorem const_rule [Inhabited F₂] (hy : RealToFloatVal (y : R₂) (y' : F₂)) :
+     RealToFloatFun (fun x : R₁ => y) (fun x : F₁ => y') := ⟨⟩
+
 theorem comp_rule (f : R₂ → R₃) (g : R₁ → R₂) (f' g')
     (hf : RealToFloatFun f f') (hg : RealToFloatFun g g') :
     RealToFloatFun (fun x => f (g x)) (fun x => f' (g' x)) := ⟨⟩
+
 theorem let_rule (g : R₁ → R₂) (f : R₂ → R₁ → R₃) (f' g')
     (hg : RealToFloatFun g g') (hf : RealToFloatFun (fun x : R₂×R₁ => f x.1 x.2) f')  :
     RealToFloatFun (fun x => let y := g x; f y x) (fun x => let y := g' x; f' (y,x)) := ⟨⟩
@@ -79,11 +90,6 @@ def _root_.Float.safeInv (x : Float) := if x.abs ≤ Float.eps then 0 else 1 / x
 variable {R R₁ R₂ R₃ F F₁ F₂ F₃ : Type}
   [RealToFloatType R F] [RealToFloatType R₁ F₁] [RealToFloatType R₂ F₂] [RealToFloatType R₃ F₃]
 
-@[data_synth]
-theorem HAdd.hAdd.arg_a0a1.RealToFloatFun_rule'
-  (f g : R → ℝ) (f' g') (hf : RealToFloatFun f f') (hg : RealToFloatFun g g') :
-  RealToFloatFun (fun x : R => f x + g x) (fun x => f' x + g' x) := ⟨⟩
-
 
 @[data_synth]
 theorem Prod.fst.arg_self.RealToFloatFun_rule'
@@ -95,21 +101,128 @@ theorem Prod.snd.arg_self.RealToFloatFun_rule'
   (f : R → R₁×R₂) (f') (hf : RealToFloatFun f f') :
   RealToFloatFun (fun x : R => (f x).2) (fun x => (f' x).2) := ⟨⟩
 
-theorem HAdd.hAdd.arg_a0a1.RealToFloatFun_rule : RealToFloatFun (fun x : ℝ×ℝ => x.1 + x.2) (fun x => x.1 + x.2) := ⟨⟩
-theorem HSub.hSub.arg_a0a1.RealToFloatFun_rule : RealToFloatFun (fun x : ℝ×ℝ => x.1 - x.2) (fun x => x.1 - x.2) := ⟨⟩
-theorem HMul.hMul.arg_a0a1.RealToFloatFun_rule : RealToFloatFun (fun x : ℝ×ℝ => x.1 * x.2) (fun x => x.1 * x.2) := ⟨⟩
-theorem HDiv.hDiv.arg_a0a1.RealToFloatFun_rule : RealToFloatFun (fun x : ℝ×ℝ => x.1 / x.2) (fun x => x.1 / x.2) := ⟨⟩
-
-theorem Neg.neg.arg_a0a1.RealToFloatFun_rule : RealToFloatFun (fun x : ℝ => -x) (fun x => -x) := ⟨⟩
-theorem Inv.inv.arg_a0a1.RealToFloatFun_rule : RealToFloatFun (fun x : ℝ => x⁻¹) (fun x => 1/x) := ⟨⟩
-
-theorem Eq.arg_a0a1.RealToFloatFun_rule : RealToFloatFun (fun x : ℝ×ℝ => x.1 = x.2) (fun x => (x.1 - x.2).abs ≤ Float.eps) := ⟨⟩
-theorem LT.lt.arg_a0a1.RealToFloatFun_rule : RealToFloatFun (fun x : ℝ×ℝ => x.1 < x.2) (fun x => x.1 < x.2) := ⟨⟩
-theorem LE.le.arg_a0a1.RealToFloatFun_rule : RealToFloatFun (fun x : ℝ×ℝ => x.1 ≤ x.2) (fun x => x.1 ≤ x.2) := ⟨⟩
-theorem GT.gt.arg_a0a1.RealToFloatFun_rule : RealToFloatFun (fun x : ℝ×ℝ => x.1 > x.2) (fun x => x.1 > x.2) := ⟨⟩
-theorem GE.ge.arg_a0a1.RealToFloatFun_rule : RealToFloatFun (fun x : ℝ×ℝ => x.1 ≥ x.2) (fun x => x.1 ≥ x.2) := ⟨⟩
+@[data_synth]
+theorem realToFloatVal_notRealType [NotRealType S] (s : S) : RealToFloatVal s s := ⟨⟩
 
 
-#check (RealToFloatFun (fun x : ℝ => let y := x + x; let z := x + y; y + z + x + x + x) _)
+----------------------------------------------------------------------------------------------------
+-- Besic operations on Reals -----------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------
+
+
+@[data_synth]
+theorem HAdd.hAdd.arg_a0a1.RealToFloatFun_rule'
+  (f g : R → ℝ) (f' g') (hf : RealToFloatFun f f') (hg : RealToFloatFun g g') :
+  RealToFloatFun (fun x : R => f x + g x) (fun x => f' x + g' x) := ⟨⟩
+
+@[data_synth]
+theorem HMul.hMul.arg_a0a1.RealToFloatFun_rule'
+  (f g : R → ℝ) (f' g') (hf : RealToFloatFun f f') (hg : RealToFloatFun g g') :
+  RealToFloatFun (fun x : R => f x * g x) (fun x => f' x * g' x) := ⟨⟩
+
+@[data_synth]
+theorem HDiv.hDiv.arg_a0a1.RealToFloatFun_rule'
+  (f g : R → ℝ) (f' g') (hf : RealToFloatFun f f') (hg : RealToFloatFun g g') :
+  RealToFloatFun (fun x : R => f x / g x) (fun x => f' x / g' x) := ⟨⟩
+
+@[data_synth]
+theorem HPow.hPow.arg_a0a1.RealToFloatFun_rule'
+  (f g : R → ℝ) (f' g') (hf : RealToFloatFun f f') (hg : RealToFloatFun g g') :
+  RealToFloatFun (fun x : R => f x ^ g x) (fun x => f' x ^ g' x) := ⟨⟩
+
+@[data_synth]
+theorem Neg.neg.arg_a0a1.RealToFloatFun_rule'
+  (f : R → ℝ) (f') (hf : RealToFloatFun f f') :
+  RealToFloatFun (fun x : R => - f x) (fun x => - f' x) := ⟨⟩
+
+@[data_synth]
+theorem Inv.inv.arg_a0a1.RealToFloatFun_rule'
+  (f : R → ℝ) (f') (hf : RealToFloatFun f f') :
+  RealToFloatFun (fun x : R => (f x)⁻¹) (fun x => 1 / f' x) := ⟨⟩
+
+@[data_synth]
+theorem Eq.arg_a0a1.RealToFloatFun_rule'
+  (f g : R → ℝ) (f' g') (hf : RealToFloatFun f f') (hg : RealToFloatFun g g') :
+  RealToFloatFun (fun x : R => f x = g x) (fun x => (f' x - g' x).abs ≤ Float.eps) := ⟨⟩
+
+@[data_synth]
+theorem LT.lt.arg_a0a1.RealToFloatFun_rule'
+  (f g : R → ℝ) (f' g') (hf : RealToFloatFun f f') (hg : RealToFloatFun g g') :
+  RealToFloatFun (fun x : R => f x < g x) (fun x => f' x < g' x) := ⟨⟩
+
+@[data_synth]
+theorem LE.le.arg_a0a1.RealToFloatFun_rule'
+  (f g : R → ℝ) (f' g') (hf : RealToFloatFun f f') (hg : RealToFloatFun g g') :
+  RealToFloatFun (fun x : R => f x ≤ g x) (fun x => f' x ≤ g' x) := ⟨⟩
+
+@[data_synth]
+theorem GT.gt.arg_a0a1.RealToFloatFun_rule'
+  (f g : R → ℝ) (f' g') (hf : RealToFloatFun f f') (hg : RealToFloatFun g g') :
+  RealToFloatFun (fun x : R => f x > g x) (fun x => f' x > g' x) := ⟨⟩
+
+@[data_synth]
+theorem GE.ge.arg_a0a1.RealToFloatFun_rule'
+  (f g : R → ℝ) (f' g') (hf : RealToFloatFun f f') (hg : RealToFloatFun g g') :
+  RealToFloatFun (fun x : R => f x ≥ g x) (fun x => f' x ≥ g' x) := ⟨⟩
+
+@[data_synth]
+theorem One.one.RealToFloatVal_ruel : RealToFloatVal (1:ℝ) (1:Float) := ⟨⟩
+
+@[data_synth]
+theorem Zero.zero.RealToFloatVal_ruel : RealToFloatVal (0:ℝ) (0:Float) := ⟨⟩
+
+@[data_synth]
+theorem nat_coe_RealtoFloatVal_rule (n : ℕ) : RealToFloatVal (n : ℝ) (n.toFloat) := ⟨⟩
+
+@[data_synth]
+theorem OfNat.ofNat.RealtoFloatVal_rule
+    {n : ℕ} [NatCast ℝ] [Nat.AtLeastTwo n] : RealToFloatVal (OfNat.ofNat n : ℝ) (OfNat.ofNat n : Float) := ⟨⟩
+
+@[data_synth]
+theorem ofScientific.RealToFloatVal_rule :
+    RealToFloatVal (OfScientific.ofScientific n b m :ℝ) (OfScientific.ofScientific n b m :Float) := ⟨⟩
+
+
+#check WeierstrassCurve
+
+instance : RealToFloatType (WeierstrassCurve ℝ) (WeierstrassCurve Float) := ⟨⟩
+
+#eval WeierstrassCurve.Δ ⟨1.0,2.0,3.0,4.0,5.0⟩
+
+
+#check WeierstrassCurve.Projective.dblX
+
+#synth RealScalar Float
+#check WeierstrassCurve.Δ
+
+set_option trace.Meta.Tactic.data_synth true in
+#check (RealToFloatFun (WeierstrassCurve.Δ : _ → ℝ) _)
+  rewrite_by
+    simp +unfoldPartialApp only [WeierstrassCurve.Δ, WeierstrassCurve.b₂, WeierstrassCurve.b₄]
+    simp +unfoldPartialApp only [WeierstrassCurve.b₈]
+    data_synth
+
+
+set_option pp.funBinderTypes true
+#check (RealToFloatFun (fun x : ℝ =>
+          let y := x + x;
+          let z := x + y;
+          y + z + x + x + x) _)
+  rewrite_by
+    data_synth
+
+
+set_option trace.Meta.Tactic.data_synth true in
+#check (RealToFloatFun (fun x : ℝ => (1.001 : ℝ) + 0.1 + x) _)
+  rewrite_by
+    data_synth
+
+
+#check (RealToFloatFun (fun x : ℝ => 1) _)
+  rewrite_by
+    data_synth
+
+
+#check (RealToFloatFun (fun x : ℝ => ((1:ℕ):ℝ)) _)
   rewrite_by
     data_synth
