13eb2597a0e9f98a989e2932b5f47163189cc481
lecopivo <skrivantomas@seznam.cz>
2025-01-16 12:58:34 -0500

    fix defeq issue with `Inner` and function types

--- SciLean/Analysis/Matrix.lean
+++ SciLean/Analysis/Matrix.lean
@@ -9,10 +9,11 @@ attribute [instance] Matrix.normedAddCommGroup Matrix.normedSpace
 
 variable
     {R m n α : Type*}
-    [Fintype m] [Fintype n]
+    [IndexType m] [IndexType n]
 
-instance [AddCommMonoid R] [Inner R α] : Inner R (Matrix m n α) := ⟨fun A B =>
-  Finset.univ.sum fun i => Finset.univ.sum fun j => Inner.inner (A i j) (B i j)⟩
+instance [AddCommMonoid R] [Inner R α] : Inner R (Matrix m n α) := by
+  unfold Matrix
+  infer_instance
 
 instance [RCLike R] [NormedAddCommGroup α] [AdjointSpace R α] : AdjointSpace R (Matrix m n α) where
   inner_top_equiv_norm := sorry_proof
