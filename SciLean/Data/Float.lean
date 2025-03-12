d52d17aac1e3372b237b87f5013d894d0ac238f9
lecopivo <skrivantomas@seznam.cz>
2025-03-12 12:08:08 -0400

    better implementaion of HPow Float Nat Float

--- SciLean/Data/Float.lean
+++ SciLean/Data/Float.lean
@@ -10,15 +10,8 @@ opaque tgamma (x : Float) : Float
 @[extern "scilean_float_lgamma"]
 opaque lgamma (x : Float) : Float
 
-
-partial def npow (x : Float) (n : Nat) : Float :=
-  match n with
-  | 0 => 1.0
-  | 1 => x
-  | 2 => x*x
-  | 3 => x*x*x
-  | 4 => (x*x)*(x*x)
-  | _ => go x n 1.0
+partial def npowRec (x : Float) (n : Nat) : Float :=
+  go x n 1.0
 where
   go (x : Float) (n : Nat) (r : Float) : Float :=
     match n with
@@ -30,7 +23,17 @@ where
       else
         go (x*x) (n/2) r
 
+@[inline]
+partial def npow (x : Float) (n : Nat) : Float :=
+  match n with
+  | 0 => 1.0
+  | 1 => x
+  | 2 => x*x
+  | 3 => x*x*x
+  | 4 => (x*x)*(x*x)
+  | _ => x.npowRec n
 
+@[inline]
 partial def zpow (x : Float) (n : Int) : Float :=
   if 0 ≤ n then
     x.npow n.toNat
