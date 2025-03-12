ffd36c11d62d9301a58cc965a5cd562c65c07302
lecopivo <skrivantomas@seznam.cz>
2025-03-12 12:06:32 -0400

    get float from ByteArray

---
+++ SciLean/Data/ByteArray.lean
@@ -0,0 +1,5 @@
+
+/-- Get `i`-th float out of `ByteArray` if interpreted as `FloatArray` -/
+@[extern c inline "((double*)(lean_sarray_cptr(#1)))[#2]"]
+-- @[extern "scilean_byte_array_uget_float"]
+opaque ByteArray.ugetFloat (a : @& ByteArray) (i : USize) (hi : i.toNat*8 + 7 < a.size) : Float
