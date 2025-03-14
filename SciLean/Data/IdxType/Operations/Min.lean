70420f4ccf99b438b708f16d28fb8a63434ab649
lecopivo <skrivantomas@seznam.cz>
2025-03-14 11:46:49 -0400

    derivative rules for `IdxType.sum/min/max`

---
+++ SciLean/Data/IdxType/Operations/Min.lean
@@ -0,0 +1,76 @@
+import SciLean.Analysis.Calculus.HasRevFDeriv
+import SciLean.Analysis.Calculus.HasFwdFDeriv
+
+
+namespace SciLean.IdxType
+
+set_option linter.unusedVariables false
+
+variable
+  {𝕜 : Type u} [RealScalar 𝕜] [Inhabited 𝕜]
+  {W : Type*} [NormedAddCommGroup W] [NormedSpace 𝕜 W]
+  {U : Type*} [NormedAddCommGroup U] [AdjointSpace 𝕜 U]
+  -- {X : Type*} [NormedAddCommGroup X] [NormedSpace 𝕜 X]
+  {I : Type v} {nI : ℕ} [IdxType I nI] [IdxType.Fold'.{v,u} I] [IdxType.Fold'.{v,max u v} I] [Inhabited I]
+
+
+@[fun_prop]
+theorem min.arg_f.Differentiable_rule
+    (f : W → I → 𝕜) (hf : Function.IsConstant (fun w => argMin (f w))) :
+    Differentiable 𝕜 (fun w => min (f w)) := sorry_proof
+
+-- @[data_synth]
+-- theorem min.arg_f.HasFDerivAt_comp_rule (w₀ : W)
+--     (f : W → I → X) (f' : I → _) (hf : ∀ i, HasFDerivAt (𝕜:=𝕜) (f · i) (f' i) w₀):
+--     HasFDerivAt
+--       (fun w  => min (f w))
+--       (fun dw =>L[𝕜] ∑ᴵ i, f' i dw) w₀ := by
+--   sorry_proof
+
+
+@[data_synth]
+theorem min.arg_f.HasFwdDeriv_rule
+    (f : W → I → 𝕜) (f' : I → W → W → 𝕜×𝕜)
+    (hf : ∀ i, HasFwdFDeriv 𝕜 (f · i) (f' i)) (hf' : (fun w => argMin (f w)).IsConstant) :
+    HasFwdFDeriv 𝕜
+      (fun w => min (f w))
+      (fun w dw =>
+        let i := argMin (f w)
+        let' (xi,dxi) := f' i w dw
+        (xi,dxi)) := by
+  sorry_proof
+
+
+
+variable
+  {W : Type*} [NormedAddCommGroup W] [AdjointSpace 𝕜 W]
+
+
+@[data_synth]
+theorem min.arg_f.HasRevDeriv_rule
+    (f : W → I → 𝕜) (f' : I → _)
+    (hf : ∀ i, HasRevFDeriv 𝕜 (f · i) (f' i)) (hf' : (fun w => argMin (f w)).IsConstant) :
+    HasRevFDeriv 𝕜
+      (fun w => min (f w))
+      (fun w =>
+        let i := argMin (f w)
+        let' (xi,dfi) := f' i w
+        (xi, fun dy =>
+          let dw := dfi dy
+          dw)) := by
+  sorry_proof
+
+
+@[data_synth]
+theorem min.arg_f.HasRevDerivUpdate_rule
+    (f : W → I → 𝕜) (f' : I → _)
+    (hf : ∀ i, HasRevFDerivUpdate 𝕜 (f · i) (f' i)) (hf' : (fun w => argMin (f w)).IsConstant) :
+    HasRevFDerivUpdate 𝕜
+      (fun w => min (f w))
+      (fun w =>
+        let i := argMin (f w)
+        let' (xi,dfi) := f' i w
+        (xi, fun dy dw =>
+          let dw := dfi dy dw
+          dw)) := by
+  sorry_proof
---
+++ SciLean/Data/IdxType/Operations/Sum.lean
@@ -0,0 +1,85 @@
+import SciLean.Analysis.Calculus.HasRevFDeriv
+import SciLean.Analysis.Calculus.HasFwdFDeriv
+
+
+namespace SciLean.IdxType
+
+set_option linter.unusedVariables false
+
+variable
+  {𝕜 : Type*} [RCLike 𝕜]
+  {W : Type*} [NormedAddCommGroup W] [NormedSpace 𝕜 W]
+  {X : Type*} [NormedAddCommGroup X] [NormedSpace 𝕜 X]
+  {I : Type*} {nI : ℕ} [IdxType I nI] [IdxType.Fold' I]
+
+
+-- @[fun_prop] -- already exists
+theorem sum.arg_f.IsContinuousLinearMap_rule' :
+    IsContinuousLinearMap 𝕜 (fun f : I → X => sum f) := by fun_prop
+
+
+@[fun_prop]
+theorem sum.arg_f.Differentiable_rule :
+    Differentiable 𝕜 (fun f : I → X => sum f) := by fun_prop
+
+
+@[data_synth]
+theorem sum.arg_f.HasFDerivAt_comp_rule (w₀ : W)
+    (f : W → I → X) (f' : I → _) (hf : ∀ i, HasFDerivAt (𝕜:=𝕜) (f · i) (f' i) w₀):
+    HasFDerivAt
+      (fun w  => sum (f w))
+      (fun dw =>L[𝕜] ∑ᴵ i, f' i dw) w₀ := by
+  sorry_proof
+
+
+-- @[data_synth] -- already exists
+theorem sum.arg_f.HasFDerivAt_simp_rule' (f₀ : I → X) :
+    HasFDerivAt (fun f : I → X => sum (fun i => f i)) (fun df : I → X =>L[𝕜] sum df) f₀ := by
+  apply hasFDerivAt_from_isContinuousLinearMap
+
+
+@[data_synth]
+theorem sum.arg_f.HasFwdDeriv_rule
+    (f : W → I → X) (f' : I → W → W → X×X) (hf : ∀ i, HasFwdFDeriv 𝕜 (f · i) (f' i)) :
+    HasFwdFDeriv 𝕜
+      (fun w => sum (f w))
+      (fun w dw =>
+        let' (x,dx) := ∑ᴵ i, f' i w dw
+        (x,dx)) := by
+  sorry_proof
+
+
+@[data_synth high]
+theorem sum.arg_f.HasRevFDeriv_rule_scalar
+    {K} [RCLike K]
+    {W} [NormedAddCommGroup W] [AdjointSpace K W]
+    {I : Type*} {nI} [IdxType I nI] [IdxType.Fold' I]
+    (f : W → I → K) {f' : I → _} (hf : ∀ i, HasRevFDerivUpdate K (f · i) (f' i))  :
+    HasRevFDeriv K
+      (fun w => sum (f w))
+      (fun w =>
+        let' (s,dw) := IdxType.fold .full (init := ((0 : K), (0:W)))
+          (fun (i : I) sdw =>
+            let' (s,dw) := sdw
+            let' (x,df) := f' i w
+            let s := s + x
+            (s, df 1 dw))
+        (s, fun dx => dx•dw)) := sorry_proof
+
+
+@[data_synth high]
+theorem sum.arg_f.HasRevFDerivUpdate_rule_scalar
+    {K} [RCLike K]
+    {W} [NormedAddCommGroup W] [AdjointSpace K W]
+    {I : Type*} {nI} [IdxType I nI] [IdxType.Fold' I]
+    (f : W → I → K) {f' : I → _} (hf : ∀ i, HasRevFDerivUpdate K (f · i) (f' i))  :
+    HasRevFDerivUpdate K
+      (fun w => sum (f w))
+      (fun w =>
+        let' (s,dw) := IdxType.fold .full (init := ((0 : K), (0:W)))
+          (fun (i : I) sdw =>
+            let' (s,dw) := sdw
+            let' (x,df) := f' i w
+            let s := s + x
+            (s, df 1 dw))
+        (s, fun dx dw' => dw' + dx•dw)) := sorry_proof
