e3c0cb91965438d94b8c880b797a5d0e89a1415d
lecopivo <skrivantomas@seznam.cz>
2025-03-21 17:22:59 -0400

    HasRevFDerivUpdate rule for mapIdxMonoAcc

--- SciLean/Data/ArrayOperations/Operations/MapIdxMonoAcc.lean
+++ SciLean/Data/ArrayOperations/Operations/MapIdxMonoAcc.lean
@@ -38,3 +38,34 @@ theorem mapIdxMonoAcc.arg_fxs.HasRevFDeriv_rule
             let dw := (xs' i w).2 dxi dw
             dw)
           dw)) := sorry_proof
+
+
+set_option linter.unusedVariables false in
+@[data_synth]
+theorem mapIdxMonoAcc.arg_fxs.HasRevFDerivUpdate_rule
+    {𝕜 : Type u} {X : Type v} {I : Type*} {Y : Type w}
+    [GetElem' X I Y] [OfFn X I Y] [LawfulOfFn X I]
+    [SetElem' X I Y] [LawfulSetElem X I]
+    {nI} [IdxType I nI] [IdxType.Fold'.{_,v} I] [RCLike 𝕜]
+    [NormedAddCommGroup X] [AdjointSpace 𝕜 X] [NormedAddCommGroup Y] [AdjointSpace 𝕜 Y]
+    [NormedAddCommGroup W] [AdjointSpace 𝕜 W] [NormedAddCommGroup Z] [AdjointSpace 𝕜 Z]
+    [IsModuleGetElem 𝕜 X I] [IsContinuousGetElem X I] [IsInnerGetElem 𝕜 X I]
+    (f : W → I → Z → Y → Y) (g : W → I → Z)  (xs : W → X) (f' g' xs' : I → _)
+    (hf : ∀ (i : I), HasRevFDerivUpdate 𝕜 (fun wzy : W×Z×Y => f wzy.1 i wzy.2.1 wzy.2.2) (f' i))
+    (hg : ∀ (i : I), HasRevFDerivUpdate 𝕜 (fun w => g w i) (g' i))
+    (hxs : ∀ (i : I), HasRevFDerivUpdate 𝕜 (fun w => (xs w)[i]) (xs' i)) :
+    HasRevFDerivUpdate 𝕜
+      (fun w => mapIdxMonoAcc (f w) (g w) (xs w))
+      (fun w =>
+        let xs := xs w
+        let r := mapIdxMonoAcc (f w) (g w) xs
+        (r, fun dy dw =>
+          let dw := IdxType.fold .full (init:=dw) (fun (i : I) dw =>
+            let xi := xs[i]
+            let dyi := dy[i]
+            let' (zi,dz') := g' i w
+            let' (dw,dzi, dxi) := (f' i (w,zi,xi)).2 dyi (dw,0)
+            let dw := dz' dzi dw
+            let dw := (xs' i w).2 dxi dw
+            dw)
+          dw)) := sorry_proof
